573f2b8ae7fb21a45a29d5dc6d1ff2f59d998d1f
Jahn Heymann <heymann@nt.upb.de>
2015-08-17 10:44:25 +0200

    SXR evaluation

---
+++ nt/evaluation/sxr.py
@@ -0,0 +1,141 @@
+import numpy
+import itertools
+
+
+def sxr(S, X):
+    """ Calculate signal to `X` ratio
+
+    :param S: Signal power
+    :param X: X power
+    :return: SXR
+    """
+    return 10 * numpy.log10(S/X)
+
+
+def input_sxr(images, noise, average_sources=True):
+    """ Calculate input SXR values according to Tran Vu.
+
+    The SXR definition is inspired by E. Vincent but the
+    exact procedure as in his paper is not used. Dang Hai Tran Vu
+    recommends this approach. Talk to Lukas Drude for details.
+
+    Take the clean images and the noise at the sensors after applying a
+    room impulse response and before mixing.
+
+    :param images: Array of unmixed but reverberated speech in time domain
+    :type images: numpy.ndarray with #Samples x #Sensors x #Speakers
+    :param noise: Array of ground truth noise
+    :type noise: numpy.ndarray with #Samples x #Sensors
+    :param average_sources: Logical if SXR is average of speakers
+    :type average_sources: bool
+
+    :return: SDR, SIR, SNR
+    """
+
+    D = images.shape[1]  # Number of sensors
+    K = images.shape[2]  # Number of speakers
+
+    S = numpy.zeros((K, D))  # Signal power
+    I = numpy.zeros((K, D))  # Interference power
+    N = numpy.zeros(D)  # Noise power
+
+    for d in range(D):
+        for k in range(K):
+            S[k, d] = numpy.var(images[:, d, k], axis=0)
+            I[k, d] = numpy.sum(
+                numpy.var(images[:, d, [n for n in range(K) if n != k]],
+                          axis=0))
+        N[d] = numpy.var(noise[:, d], axis=0)
+
+    if average_sources:
+        S = numpy.mean(S)
+        I = numpy.mean(I)
+    else:
+        S = numpy.mean(S, axis=1)
+        I = numpy.mean(I, axis=1)
+    N = numpy.mean(N)
+
+    SDR = sxr(S, I+N)
+    SIR = sxr(S, I)
+    SNR = sxr(S, N)
+
+    return SDR, SIR, SNR
+
+
+def output_sxr(image_contribution, noise_contribution, average_sources=True):
+    """ Calculate output SXR values.
+
+    The SXR definition is inspired by E. Vincent but the
+    exact procedure as in his paper is not used. Dang Hai Tran Vu
+    recommends this approach. Talk to Lukas Drude for details.
+
+    The output signal of the system under test can be decomposed in
+    contributions due to the speakers and noise. The two input signals
+    are the processed images and noise by the complete separation
+    system.
+
+    Use the mixed signals to run your algorithm. Save the estimated separation
+    parameters (i.e. beamforming vectors, gain functions, ...) and apply the
+    algorithm with the fixed paramters to the clean images and ground truth
+    noise separately. Evaluate the results with this function to obtain
+    intrusive SXR measures.
+
+    :param image_contribution:  Put the clean images into the separation
+      algorithm with fixed parameters. The output of the separation algorithm
+      can now be used as imageContribution.
+    :type image_contribution: #Samples x #sourceSpeakers x #targetSpeakers
+    :param noise_contribution: Put the ground truth into the separation
+      algorithm with fixed parameters. The output is noiseContribution.
+    :type noise_contribution: #Samples times #targetSpeakers
+    :param average_sources: Scalar logical if SXR is average of speakers;
+      optional (default: true). If set to true, SXR-values are scalars.
+
+    :return SDR: #Sources times 1 vector of Signal to Distortion Ratios
+    :return SIR: #Sources times 1 vector of Signal to Interference Ratios
+    :return SNR: #Sources times 1 vector of Signal to Noise Ratios
+   """
+
+    K_source = image_contribution.shape[1]
+    K_target = image_contribution.shape[2]  # Number of target speaker
+
+    S = numpy.zeros((K_source, K_target))
+    N = numpy.zeros(K_target)
+
+    for k_target in range(K_target):
+        for k_source in range(K_source):
+            S[k_source, k_target] = numpy.var(
+                image_contribution[:, k_source, k_target], axis=0
+            )
+        N[k_target] = numpy.var(noise_contribution[:, k_target], axis=0)
+
+    all_permutations = \
+        numpy.array(list(itertools.permutations(range(K_target))))
+
+    mutal_power = numpy.zeros(all_permutations.shape[0])
+
+    for p in range(all_permutations.shape[0]):
+        for k_target in range(K_target):
+            mutal_power[p] = mutal_power[p] + \
+                S[all_permutations[p, k_target], k_target]
+
+    max_idx = numpy.argmax(mutal_power)
+    permutation = all_permutations[max_idx]
+
+    SS = numpy.zeros(K_target)
+    II = numpy.zeros(K_target)
+    for k_target in range(K_target):
+        SS[k_target] = S[k_target, permutation[k_target]]
+        II[k_target] = numpy.sum(
+            S[[n for n in range(K_target) if n != k_target],
+              permutation[k_target]])
+
+    if average_sources:
+        SS = numpy.mean(SS)
+        II = numpy.mean(II)
+    N = numpy.mean(N)
+
+    SDR = sxr(SS, II+N)
+    SIR = sxr(SS, II)
+    SNR = sxr(SS, N)
+
+    return SDR, SIR, SNR
