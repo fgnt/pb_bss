9581a8a8b6e91b200be71ef1958dba5de9212b5b
Janek Ebbers <ebbers@mail.upb.de>
2015-08-27 16:40:11 +0200

    compare mvdr with matlab, introduced cached_property to avoid multiple matlab processes, removed vector_H_vector since can be imported, removed vector normalization from speech_enhancement to utils and thus removed testcase

--- tests/speech_enhancement_test/test_beamformer.py
+++ tests/speech_enhancement_test/test_beamformer.py
@@ -7,11 +7,21 @@ from pymatbridge import Matlab
 from nt.speech_enhancement.beamformer import get_pca_vector
 from nt.speech_enhancement.beamformer import get_mvdr_vector
 from nt.speech_enhancement.beamformer import get_gev_vector
-from nt.speech_enhancement.beamformer import normalize_vector_to_unit_length
-
-from os import environ
-matlab = unittest.skipUnless(environ.get('TEST_MATLAB'),'matlab-test')
 from os import path
+from os import environ
+from cached_property import cached_property
+from nt.utils import math
+
+class Mlab():
+    @cached_property
+    def process(self):
+        mlab_process = Matlab('nice -n 3 /net/ssd/software/MATLAB/R2015a/bin/matlab -nodisplay -nosplash')
+        mlab_process.start()
+        _ = mlab_process.run_code('run /net/home/ldrude/Projects/2015_python_matlab/matlab/startup.m')
+        return mlab_process
+
+# define decorator to skip matlab_tests
+matlab = unittest.skipUnless(environ.get('TEST_MATLAB'),'matlab-test')
 
 class TestBeamformerMethods(unittest.TestCase):
     @classmethod
@@ -32,57 +42,54 @@ class TestBeamformerMethods(unittest.TestCase):
         self.W_pca = get_pca_vector(self.Phi_XX)
         self.W_mvdr = get_mvdr_vector(self.W_pca, self.Phi_NN)
         self.W_gev = get_gev_vector(self.Phi_XX, self.Phi_NN)
-        if environ.get('TEST_MATLAB'):
-            self.mlab = Matlab('nice -n 3 /net/ssd/software/MATLAB/R2015a/bin/matlab -nodisplay -nosplash')
-            self.mlab.start()
-            _ = self.mlab.run_code('run /net/home/ldrude/Projects/2015_python_matlab/matlab/startup.m')
+        self.mlab = Mlab()
 
-    @matlab
+    #@matlab
     def test_compare_PSD_without_mask(self):
-        self.mlab.set_variable('Y', self.Y_bf)
-        self.mlab.run_code('Phi = random.covariance(Y, [], 3, 2);')
-        Phi_matlab = self.mlab.get_variable('Phi')
+        mlab = self.mlab.process
+        mlab.set_variable('Y', self.Y_bf)
+        mlab.run_code('Phi = random.covariance(Y, [], 3, 2);')
+        Phi_matlab = mlab.get_variable('Phi')
         Phi = get_power_spectral_density_matrix(self.Y_bf)
         tc.assert_allclose(Phi, Phi_matlab, atol=1e-4)
 
-    @matlab
+    #@matlab
     def test_compare_PSD_with_mask(self):
-        self.mlab.set_variable('Y', self.Y_bf)
-        self.mlab.set_variable('ibm', self.ibm_N_bf[:, np.newaxis, :].astype(np.float))
-        self.mlab.run_code('Phi = random.covariance(Y, ibm, 3, 2);')
-        Phi_matlab = self.mlab.get_variable('Phi')
+        mlab = self.mlab.process
+        mlab.set_variable('Y', self.Y_bf)
+        mlab.set_variable('ibm', self.ibm_N_bf[:, np.newaxis, :].astype(np.float))
+        mlab.run_code('Phi = random.covariance(Y, ibm, 3, 2);')
+        Phi_matlab = mlab.get_variable('Phi')
         Phi = get_power_spectral_density_matrix(self.Y_bf, self.ibm_N_bf)
         tc.assert_allclose(Phi, Phi_matlab, atol=1e-4)
 
-    @matlab
+    #@matlab
     def test_compare_PCA_beamformer(self):
-        self.mlab.set_variable('Phi_XX', self.Phi_XX)
-        self.mlab.run_code("W = bss.beamformer.pca('cleanObservationMatrix', Phi_XX);")
-        W_matlab = self.mlab.get_variable('W')
-        distance = 1 - np.abs(self.vector_H_vector(self.W_pca, W_matlab))**2
+        mlab = self.mlab.process
+        mlab.set_variable('Phi_XX', self.Phi_XX)
+        mlab.run_code("W = bss.beamformer.pca('cleanObservationMatrix', Phi_XX);")
+        W_matlab = mlab.get_variable('W')
+        distance = 1 - np.abs(math.vector_H_vector(self.W_pca, W_matlab))**2
         tc.assert_array_less(distance, 1e-6)
 
     def test_mvdr_beamformer(self):
-        tc.assert_allclose(self.vector_H_vector(self.W_pca, self.W_mvdr), 1)
+        tc.assert_allclose(math.vector_H_vector(self.W_pca, self.W_mvdr), 1)
 
-    @matlab
+    #@matlab
     def test_compare_mvdr_beamformer(self):
-        # missing comparison to matlab result
-        tc.assert_array_equal(0, 1)
+        mlab = self.mlab.process
+        mlab.set_variable('Phi_NN', self.Phi_NN)
+        mlab.set_variable('lookDirection', self.W_pca)
+        mlab.run_code("W = bss.beamformer.mvdr('noiseMatrix', Phi_NN, 'lookDirection', lookDirection);")
+        W_matlab = mlab.get_variable('W')
+        tc.assert_cosine_similarity(W_matlab, self.W_mvdr)
 
-    @matlab
+    #@matlab
     def test_compar_gev_beamformer(self):
-        self.mlab.set_variable('Phi_XX', self.Phi_XX)
-        self.mlab.set_variable('Phi_NN', self.Phi_NN)
-        self.mlab.run_code("W = bss.beamformer.gev('cleanObservationMatrix', Phi_XX, 'noiseMatrix', Phi_NN);")
-        W_matlab = self.mlab.get_variable('W')
-        distance = 1 - np.abs(self.vector_H_vector(self.W_gev, W_matlab))**2
+        mlab = self.mlab.process
+        mlab.set_variable('Phi_XX', self.Phi_XX)
+        mlab.set_variable('Phi_NN', self.Phi_NN)
+        mlab.run_code("W = bss.beamformer.gev('cleanObservationMatrix', Phi_XX, 'noiseMatrix', Phi_NN);")
+        W_matlab = mlab.get_variable('W')
+        distance = 1 - np.abs(math.vector_H_vector(self.W_gev, W_matlab))**2
         tc.assert_array_less(distance, 1e-6)
-
-    def test_Unit_length_normalization(self):
-        W_normalized = normalize_vector_to_unit_length(self.W_gev)
-        tc.assert_allclose(self.vector_H_vector(W_normalized, W_normalized), 1)
-
-    @staticmethod
-    def vector_H_vector(x, y):
-        return np.einsum('...a,...a->...', x.conj(), y)
