a9244432a7096fe05be2753a39da4977d284ff12
Christoph <cbj@mail.uni-paderborn.de>
2015-10-07 16:02:57 +0200

    add multispeaker test for lcmv beamformer

--- tests/speech_enhancement_test/test_beamformer.py
+++ tests/speech_enhancement_test/test_beamformer.py
@@ -6,18 +6,30 @@ from nt.speech_enhancement.beamformer import get_power_spectral_density_matrix
 from nt.speech_enhancement.beamformer import get_pca_vector
 from nt.speech_enhancement.beamformer import get_mvdr_vector
 from nt.speech_enhancement.beamformer import get_gev_vector
+from nt.speech_enhancement.beamformer import get_lcmv_vector
 from os import path
 from nt.utils.math_ops import vector_H_vector
 from nt.utils.matlab import Mlab, matlab_test
+from nt.speech_enhancement.mask_estimation import simple_ideal_soft_mask
+
+# uncomment, if you want to test matlab functions
+# matlab_test = unittest.skipUnless(True,'matlab-test')
 
 class TestBeamformerMethods(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         datafile = path.join(path.dirname(path.realpath(__file__)), 'data.npz')
+        datafile_multi_speaker = path.join(path.dirname(path.realpath(__file__)), 'data_multi_speaker.npz')
+
+        self.mlab = Mlab()
+
+        if not path.exists(datafile_multi_speaker):
+            self.generate_source_file_with_matlab(mlab=self.mlab)
+
         with np.load(datafile) as data:
             X = data['X']
-            Y= data['Y']
-            N=data['N']
+            Y = data['Y']
+            N = data['N']
         ibm = ideal_binary_mask(X[:, 4, :], N[:, 4, :])
         self.Y_bf, self.X_bf, self.N_bf = Y.T, X.T, N.T
         self.ibm_X_bf = ibm[0].T
@@ -30,7 +42,76 @@ class TestBeamformerMethods(unittest.TestCase):
         self.W_pca = get_pca_vector(self.Phi_XX)
         self.W_mvdr = get_mvdr_vector(self.W_pca, self.Phi_NN)
         self.W_gev = get_gev_vector(self.Phi_XX, self.Phi_NN)
-        self.mlab = Mlab()
+
+        with np.load(datafile_multi_speaker) as data:
+            X = data['X'] # K F D T
+            Y = data['Y'] # F D T
+            N = data['N'] # F D T
+            self.data_multi_speaker = {'X':data['X'], 'Y':data['Y'], 'N':data['N']}
+
+
+        X_mask, N_mask = simple_ideal_soft_mask(X, N, source_dim=0, feature_dim=-2, tuple_output=True)
+
+        Phi_XX = get_power_spectral_density_matrix(Y, X_mask, source_dim=0) # K F D D
+        Phi_NN = get_power_spectral_density_matrix(Y, N_mask) # F D D
+
+        W_pca = get_pca_vector(Phi_XX)
+        W_mvdr = get_mvdr_vector(W_pca, Phi_NN)
+        W_gev = np.zeros_like(W_mvdr)
+        print(Phi_XX.shape)
+        print(W_gev.shape)
+        W_gev[0, :, :] = get_gev_vector(Phi_XX[0, :, :, :], Phi_NN)
+        W_gev[1, :, :] = get_gev_vector(Phi_XX[1, :, :, :], Phi_NN)
+
+        W_lcmv = get_lcmv_vector(W_pca, [1, 0], Phi_NN)
+
+    @staticmethod
+    def generate_source_file_with_matlab(mlab = Mlab()):
+        import nt.transform as transform
+
+        # ToDo: replace with Python funktions (current missing)
+        mlab.run_code('D = 6;')  # Number of microphones
+        mlab.run_code('K = 2;')  # Number of speakers
+        mlab.run_code('tSignal = 5;')  # Signal length in seconds
+        mlab.run_code('seed = 1;')
+        mlab.run_code('SNR = 15;')
+        mlab.run_code('soundDecayTime = 0.16;')
+        mlab.run_code('samplingRate = 16000;')
+        mlab.run_code("noiseType = 'whiteGaussian';")
+        mlab.run_code('rirFilterLength = 2^14;')
+        mlab.run_code('fftSize = 2^10;')
+        mlab.run_code('fftShiftFactor = 1/4;')
+        mlab.run_code('analysisWindowHandle = @(x) blackman(x);')
+
+        mlab.run_code('sourceDoA =  degtorad([30, -30, 60, -60, 90, -90, 15, -15, 45, -45, 75, -75, 0]);')
+        mlab.run_code('sourceDoA = sourceDoA(1:K);')
+        mlab.run_code('rir = zeros(rirFilterLength, 8, numel(sourceDoA));')
+        mlab.run_code('for ii = 1 : numel(sourceDoA)'
+                      '    [rir(:, :, ii), sensorPositions] = reverb.acquireMIRD(1, 1, sourceDoA(ii), samplingRate, rirFilterLength, soundDecayTime);'
+                      'end')
+        mlab.run_code('sensorsID = [4 5 3 6 2 7 1 8];')
+        mlab.run_code('sensorsID = sensorsID(1:D);')
+        mlab.run_code('sensorsID = sort(sensorsID);')
+        mlab.run_code('sensorPositions = sensorPositions(:, sensorsID);')
+        mlab.run_code('rir = rir(:, sensorsID, :);')
+
+        mlab.run_code('speakers = database.acquireSignals(tSignal, samplingRate,  K, seed);')
+        mlab.run_code("noise = database.noise(tSignal, samplingRate, D, seed, 'noiseType', noiseType, 'sensorPositions', sensorPositions);")
+
+        mlab.run_code("[~, speakers, noise] = bss.generate.convolutiveMixture(speakers, rir, noise, SNR, 'sourceScaled');")
+
+        speakers = mlab.get_variable('speakers')
+        noise = mlab.get_variable('noise')
+
+        Speakers = transform.stft(speakers)
+        Noise = transform.stft(noise)
+
+        Y = np.sum(np.concatenate((Speakers, Noise[:, :, :, np.newaxis]), axis=3), axis=3).transpose(1, 2, 0).copy()
+        X = Speakers.transpose(3, 1, 2, 0).copy()
+        N = Noise.transpose(1, 2, 0).copy()
+
+        datafile_multi_speaker = path.join(path.dirname(path.realpath(__file__)), 'data_multi_speaker.npz')
+        np.savez(datafile_multi_speaker, X=X, Y=Y, N=N)
 
     @matlab_test
     def test_compare_PSD_without_mask(self):
@@ -79,3 +160,74 @@ class TestBeamformerMethods(unittest.TestCase):
         mlab.run_code("W = bss.beamformer.gev('cleanObservationMatrix', Phi_XX, 'noiseMatrix', Phi_NN);")
         W_matlab = mlab.get_variable('W')
         tc.assert_cosine_similarity(W_matlab, self.W_gev)
+
+    @matlab_test
+    def test_compare_lcmv_beamformer(self):
+        from nt.speech_enhancement.beamformer import apply_beamforming_vector
+        from nt.transform.module_stft import istft_loop
+        from nt.evaluation import sxr
+
+        data = self.data_multi_speaker
+        X = data['X'] # K F D T
+        Y = data['Y'] # F D T
+        N = data['N'] # F D T
+
+        # Caculate masks
+        # X_mask.shape = (2, 513, 316)
+        # N_mask.shape = (513, 316)
+        X_mask, N_mask = simple_ideal_soft_mask(X, N, source_dim=0, feature_dim=-2, tuple_output=True)
+
+        # Phi_XX.shape = (2, 513, 6, 6)
+        # Phi_NN.shape = (513, 6, 6)
+        Phi_XX = get_power_spectral_density_matrix(Y, X_mask, source_dim=0) # K F D D
+        Phi_NN = get_power_spectral_density_matrix(Y, N_mask) # F D D
+
+        # W_pca.shape = (2, 513, 6)
+        W_pca = get_pca_vector(Phi_XX)
+
+        # W_lcmv1.shape = (513, 6)
+        # W_lcmv2.shape = (513, 6)
+        # W_lcmv.shape = (2, 513, 6)
+        W_lcmv1 = get_lcmv_vector(W_pca, [1, 0], Phi_NN)
+        W_lcmv2 = get_lcmv_vector(W_pca, [0, 1], Phi_NN)
+        W_lcmv = np.array(([W_lcmv1, W_lcmv2]))
+
+        # W_pca_tmp.shape = (513, 2, 6)
+        W_pca_tmp = W_pca.transpose(1, 2, 0)
+        Phi_NN_tmp = Phi_NN
+
+        mlab = self.mlab
+        mlab.set_variable('W_pca', W_pca_tmp)
+        mlab.set_variable('Phi_NN', Phi_NN_tmp)
+        mlab.run_code_print('size(W_pca)')
+        mlab.run_code_print('size(Phi_NN)')
+        mlab.run_code("[W(:, :, 1), failing] = bss.beamformer.lcmv('observationMatrix', Phi_NN, 'lookDirection', W_pca, 'responseVector', [1 0]);")
+        mlab.run_code("W(:, :, 2) = bss.beamformer.lcmv('observationMatrix', Phi_NN, 'lookDirection', W_pca, 'responseVector', [0 1]);")
+        W_matlab = mlab.get_variable('W')
+        failing = mlab.get_variable('failing')
+        print(np.sum(failing))
+        assert(np.sum(failing) == 0.0)
+
+        def sxr_output(W):
+            Shat = np.zeros((2, 2, 513, 316), dtype=complex)
+            Shat[0, 0, :, :] = apply_beamforming_vector(W[0, :, :], X[0, :, :, :])
+            Shat[1, 1, :, :] = apply_beamforming_vector(W[1, :, :], X[1, :, :, :])
+            Shat[0, 1, :, :] = apply_beamforming_vector(W[1, :, :], X[0, :, :, :])
+            Shat[1, 0, :, :] = apply_beamforming_vector(W[0, :, :], X[1, :, :, :])
+
+            Nhat = np.zeros((2, 513, 316), dtype=complex)
+            Nhat[0, :, :] = apply_beamforming_vector(W[0, :, :], N)
+            Nhat[0, :, :] = apply_beamforming_vector(W[0, :, :], N)
+            shat = istft_loop(Shat, time_dim=-1, freq_dim=-2)
+            nhat = istft_loop(Nhat, time_dim=-1, freq_dim=-2)
+            return sxr.output_sxr(shat.transpose(2, 0, 1), nhat.transpose())
+
+        W_matlab_tmp = W_matlab.transpose(2, 0, 1)
+        W_lcmv_tmp = W_lcmv
+
+        sxr_matlab = sxr_output(W_matlab_tmp)
+        sxr_py = sxr_output(W_lcmv_tmp)
+
+        tc.assert_almost_equal(sxr_matlab, sxr_py)
+
+        tc.assert_cosine_similarity(W_matlab_tmp, W_lcmv_tmp)
