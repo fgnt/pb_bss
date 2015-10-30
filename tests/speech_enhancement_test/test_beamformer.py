5b80b86d08b73fff390a40f7624be35d0fd5f816
Christoph Boeddeker <boeddeker@ntws25.nt.uni-paderborn.de>
2015-10-30 16:17:35 +0100

    improved get_power_spectral_density_matrix and add testcases

--- tests/speech_enhancement_test/test_beamformer.py
+++ tests/speech_enhancement_test/test_beamformer.py
@@ -1,19 +1,118 @@
 import unittest
-import nt.testing as tc
+from os import path
+
 import numpy as np
+
+import nt.testing as tc
 from nt.speech_enhancement.mask_estimation import estimate_IBM as ideal_binary_mask
 from nt.speech_enhancement.beamformer import get_power_spectral_density_matrix
 from nt.speech_enhancement.beamformer import get_pca_vector
 from nt.speech_enhancement.beamformer import get_mvdr_vector
 from nt.speech_enhancement.beamformer import get_gev_vector
 from nt.speech_enhancement.beamformer import get_lcmv_vector
-from os import path
 from nt.utils.math_ops import vector_H_vector
 from nt.utils.matlab import Mlab, matlab_test
 from nt.speech_enhancement.mask_estimation import simple_ideal_soft_mask
 
+
 # uncomment, if you want to test matlab functions
 # matlab_test = unittest.skipUnless(True,'matlab-test')
+
+def rand(*shape, data_type):
+    if not shape:
+        shape = (1,)
+    elif isinstance(shape[0], tuple):
+        shape = shape[0]
+
+    def uniform(data_type_local):
+        return np.random.uniform(-1, 1, shape).astype(data_type_local)
+
+    if data_type in (np.float32, np.float64):
+        return uniform(data_type)
+    elif data_type is np.complex64:
+        return uniform(np.float32) + 1j * uniform(np.float32)
+    elif data_type is np.complex128:
+        return uniform(np.float64) + 1j * uniform(np.float64)
+
+
+class TestPSDMatrix(unittest.TestCase):
+    # or covariance matrix
+
+    F, T, D, K = 51, 31, 6, 2
+
+    def generate_date(self, x_shape, mask_shape):
+        x = rand(x_shape, data_type=np.complex128)
+        mask = np.random.uniform(0, 1, mask_shape)
+        mask = mask / np.sum(mask, axis=0, keepdims=True)
+        return x, mask
+
+    def gererate_and_verify_psd(self, x_shape, mask_shape, psd_shape=None):
+        x, mask = self.generate_date(x_shape, mask_shape)
+        if mask_shape is None:
+            psd = get_power_spectral_density_matrix(x)
+        else:
+            psd = get_power_spectral_density_matrix(x, mask)
+        if psd_shape is not None:
+            tc.assert_equal(psd.shape, psd_shape)
+        tc.assert_hermitian(psd)
+        tc.assert_positive_semidefinite(psd)
+
+    def test_PSD_without_mask(self):
+        self.gererate_and_verify_psd((self.D, self.T), None, psd_shape=(self.D, self.D))
+
+    def test_PSD_with_mask(self):
+        self.gererate_and_verify_psd((self.D, self.T), (self.T,), psd_shape=(self.D, self.D))
+
+    def test_PSD_with_mask_with_source(self):
+        self.gererate_and_verify_psd((self.D, self.T), (self.K, self.T,), psd_shape=(self.K, self.D, self.D))
+
+    def test_PSD_with_mask_independent_dim(self):
+        self.gererate_and_verify_psd((self.F, self.D, self.T), (self.F, self.T,), psd_shape=(self.F, self.D, self.D))
+        self.gererate_and_verify_psd((self.F, self.F, self.D, self.T), (self.F, self.F, self.T,),
+                                     psd_shape=(self.F, self.F, self.D, self.D))
+
+    def test_PSD_with_mask_independent_dim_with_source(self):
+        self.gererate_and_verify_psd((self.F, self.D, self.T), (self.F, self.K, self.T,),
+                                     psd_shape=(self.F, self.K, self.D, self.D))
+        self.gererate_and_verify_psd((self.F, self.F, self.D, self.T), (self.F, self.F, self.K, self.T,),
+                                     psd_shape=(self.F, self.F, self.K, self.D, self.D))
+
+    def test_PSD_without_mask_independent_dim(self):
+        self.gererate_and_verify_psd((self.F, self.D, self.T), None, psd_shape=(self.F, self.D, self.D))
+        self.gererate_and_verify_psd((self.F, self.F, self.D, self.T), (self.F, self.F, self.T,),
+                                     psd_shape=(self.F, self.F, self.D, self.D))
+
+    def test_predict_output(self):
+        x, _ = self.generate_date((self.D,), None)
+        x_rep = x[:, np.newaxis].repeat((self.T,), 1)
+        psd = get_power_spectral_density_matrix(x_rep)
+
+        psd_predict = x[:, np.newaxis].dot(x[np.newaxis, :].conj())
+        tc.assert_almost_equal(psd, psd_predict)
+
+    def test_predict_output_with_mask(self):
+        x, _ = self.generate_date((self.D,), None)
+        x_rep = x[:, np.newaxis].repeat((self.T,), 1)
+        mask = np.ones((self.T,))
+        psd = get_power_spectral_density_matrix(x_rep, mask)
+        psd2 = get_power_spectral_density_matrix(x_rep, mask * 2)
+        psd3 = get_power_spectral_density_matrix(x_rep, mask * 0.5)
+
+        psd_predict = x[:, np.newaxis].dot(x[np.newaxis, :].conj())
+        tc.assert_almost_equal(psd, psd_predict)
+        tc.assert_almost_equal(psd, psd2)
+        tc.assert_almost_equal(psd, psd3)
+
+    def test_different_valued_masks_output(self):
+        x, mask = self.generate_date((self.F, self.D, self.T), (self.F, self.T,))
+
+        psd = get_power_spectral_density_matrix(x, mask)
+        psd2 = get_power_spectral_density_matrix(x, mask*2)
+        psd3 = get_power_spectral_density_matrix(x, mask*0.5)
+
+        tc.assert_almost_equal(psd, psd2)
+        tc.assert_almost_equal(psd, psd3)
+
 
 class TestBeamformerMethods(unittest.TestCase):
     @classmethod
@@ -38,22 +137,21 @@ class TestBeamformerMethods(unittest.TestCase):
         self.ibm_N_bf_th = np.maximum(self.ibm_N_bf, 1e-4)
         self.Phi_XX = get_power_spectral_density_matrix(self.Y_bf, self.ibm_X_bf_th)
         self.Phi_NN = get_power_spectral_density_matrix(self.Y_bf, self.ibm_N_bf_th)
-        self.Phi_NN = self.Phi_NN + np.tile(1e-10*np.eye(self.Phi_NN.shape[1]), (self.Phi_NN.shape[0], 1, 1))
+        self.Phi_NN = self.Phi_NN + np.tile(1e-10 * np.eye(self.Phi_NN.shape[1]), (self.Phi_NN.shape[0], 1, 1))
         self.W_pca = get_pca_vector(self.Phi_XX)
         self.W_mvdr = get_mvdr_vector(self.W_pca, self.Phi_NN)
         self.W_gev = get_gev_vector(self.Phi_XX, self.Phi_NN)
 
         with np.load(datafile_multi_speaker) as data:
-            X = data['X'] # K F D T
-            Y = data['Y'] # F D T
-            N = data['N'] # F D T
-            self.data_multi_speaker = {'X':data['X'], 'Y':data['Y'], 'N':data['N']}
-
+            X = data['X']  # K F D T
+            Y = data['Y']  # F D T
+            N = data['N']  # F D T
+            self.data_multi_speaker = {'X': data['X'], 'Y': data['Y'], 'N': data['N']}
 
         X_mask, N_mask = simple_ideal_soft_mask(X, N, source_dim=0, feature_dim=-2, tuple_output=True)
 
-        Phi_XX = get_power_spectral_density_matrix(Y, X_mask, source_dim=0) # K F D D
-        Phi_NN = get_power_spectral_density_matrix(Y, N_mask) # F D D
+        Phi_XX = get_power_spectral_density_matrix(Y, X_mask, source_dim=0)  # K F D D
+        Phi_NN = get_power_spectral_density_matrix(Y, N_mask)  # F D D
 
         W_pca = get_pca_vector(Phi_XX)
         W_mvdr = get_mvdr_vector(W_pca, Phi_NN)
@@ -66,7 +164,7 @@ class TestBeamformerMethods(unittest.TestCase):
         W_lcmv = get_lcmv_vector(W_pca, [1, 0], Phi_NN)
 
     @staticmethod
-    def generate_source_file_with_matlab(mlab = Mlab()):
+    def generate_source_file_with_matlab(mlab=Mlab()):
         import nt.transform as transform
 
         # ToDo: replace with Python funktions (current missing)
@@ -96,9 +194,11 @@ class TestBeamformerMethods(unittest.TestCase):
         mlab.run_code('rir = rir(:, sensorsID, :);')
 
         mlab.run_code('speakers = database.acquireSignals(tSignal, samplingRate,  K, seed);')
-        mlab.run_code("noise = database.noise(tSignal, samplingRate, D, seed, 'noiseType', noiseType, 'sensorPositions', sensorPositions);")
+        mlab.run_code(
+            "noise = database.noise(tSignal, samplingRate, D, seed, 'noiseType', noiseType, 'sensorPositions', sensorPositions);")
 
-        mlab.run_code("[~, speakers, noise] = bss.generate.convolutiveMixture(speakers, rir, noise, SNR, 'sourceScaled');")
+        mlab.run_code(
+            "[~, speakers, noise] = bss.generate.convolutiveMixture(speakers, rir, noise, SNR, 'sourceScaled');")
 
         speakers = mlab.get_variable('speakers')
         noise = mlab.get_variable('noise')
@@ -168,9 +268,9 @@ class TestBeamformerMethods(unittest.TestCase):
         from nt.evaluation import sxr
 
         data = self.data_multi_speaker
-        X = data['X'] # K F D T
-        Y = data['Y'] # F D T
-        N = data['N'] # F D T
+        X = data['X']  # K F D T
+        Y = data['Y']  # F D T
+        N = data['N']  # F D T
 
         # Caculate masks
         # X_mask.shape = (2, 513, 316)
@@ -179,8 +279,8 @@ class TestBeamformerMethods(unittest.TestCase):
 
         # Phi_XX.shape = (2, 513, 6, 6)
         # Phi_NN.shape = (513, 6, 6)
-        Phi_XX = get_power_spectral_density_matrix(Y, X_mask, source_dim=0) # K F D D
-        Phi_NN = get_power_spectral_density_matrix(Y, N_mask) # F D D
+        Phi_XX = get_power_spectral_density_matrix(Y, X_mask, source_dim=0)  # K F D D
+        Phi_NN = get_power_spectral_density_matrix(Y, N_mask)  # F D D
 
         # W_pca.shape = (2, 513, 6)
         W_pca = get_pca_vector(Phi_XX)
@@ -201,12 +301,14 @@ class TestBeamformerMethods(unittest.TestCase):
         mlab.set_variable('Phi_NN', Phi_NN_tmp)
         mlab.run_code_print('size(W_pca)')
         mlab.run_code_print('size(Phi_NN)')
-        mlab.run_code("[W(:, :, 1), failing] = bss.beamformer.lcmv('observationMatrix', Phi_NN, 'lookDirection', W_pca, 'responseVector', [1 0]);")
-        mlab.run_code("W(:, :, 2) = bss.beamformer.lcmv('observationMatrix', Phi_NN, 'lookDirection', W_pca, 'responseVector', [0 1]);")
+        mlab.run_code(
+            "[W(:, :, 1), failing] = bss.beamformer.lcmv('observationMatrix', Phi_NN, 'lookDirection', W_pca, 'responseVector', [1 0]);")
+        mlab.run_code(
+            "W(:, :, 2) = bss.beamformer.lcmv('observationMatrix', Phi_NN, 'lookDirection', W_pca, 'responseVector', [0 1]);")
         W_matlab = mlab.get_variable('W')
         failing = mlab.get_variable('failing')
         print(np.sum(failing))
-        assert(np.sum(failing) == 0.0)
+        assert (np.sum(failing) == 0.0)
 
         def sxr_output(W):
             Shat = np.zeros((2, 2, 513, 316), dtype=complex)
