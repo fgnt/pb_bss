6e03bf0c1484ead23fe3047ae1ed0651b5403953
Lukas Drude <mail@lukas-drude.de>
2016-01-09 16:04:45 +0100

    Reformatted beamforming code and removed old functions, which had newer replacements

--- nt/speech_enhancement/beamformer.py
+++ nt/speech_enhancement/beamformer.py
@@ -4,44 +4,27 @@ from scipy.linalg import eigh
 from numpy.linalg import solve
 
 
-def get_power_spectral_density_matrix_old(observation, mask=None):
-    """
-    Calculates the weighted power spectral density matrix.
-
-    This does not yet work with more than one target mask.
-
-    :param observation: Complex observations with shape (bins, sensors, frames)
-    :param mask: Masks with shape (bins, frames) or (bins, 1, frames)
-    :return: PSD matrix with shape (bins, sensors, sensors)
-    """
-    bins, sensors, frames = observation.shape
-
-    if mask is None:
-        mask = np.ones((bins, frames))
-    if mask.ndim == 2:
-        mask = mask[:, np.newaxis, :]
-
-    mask /= np.maximum(np.sum(mask, axis=-1, keepdims=True), 1e-6)
-
-    psd = np.einsum('...ft,...dt,...et->...de', mask, observation, observation.conj())
-    # psd /= normalization[:, np.newaxis, np.newaxis]
-    return psd
-
-
-def get_power_spectral_density_matrix(observation, mask=None, sensor_dim=-2, source_dim=-2, time_dim=-1):
+def get_power_spectral_density_matrix(observation, mask=None, sensor_dim=-2,
+                                      source_dim=-2, time_dim=-1):
     """
     Calculates the weighted power spectral density matrix.
     It's also called covariance matrix.
-    With the dim parameters you can change the sort of the dims of the observation and mask.
+    With the dim parameters you can change the sort of the dims of the
+    observation and mask.
     But not every combination is allowed.
 
     :param observation: Complex observations with shape (..., sensors, frames)
     :param mask: Masks with shape (bins, frames) or (..., sources, frames)
     :param sensor_dim: change sensor dimension index (Default: -2)
-    :param source_dim: change source dimension index (Default: -2), source_dim = 0 means mask shape (sources, ..., frames)
-    :param time_dim:  change time dimension index (Default: -1), this index must match for mask and observation
-    :return: PSD matrix with shape (..., sensors, sensors) or (..., sources, sensors, sensors) or
-        (sources, ..., sensors, sensors) if source_dim % observation.ndim < -2 respectively mask shape (sources, ..., frames)
+    :param source_dim: change source dimension index (Default: -2),
+        source_dim = 0 means mask shape (sources, ..., frames)
+    :param time_dim:  change time dimension index (Default: -1),
+        this index must match for mask and observation
+    :return: PSD matrix with shape (..., sensors, sensors)
+        or (..., sources, sensors, sensors) or
+        (sources, ..., sensors, sensors)
+        if source_dim % observation.ndim < -2 respectively
+        mask shape (sources, ..., frames)
 
     Examples
     --------
@@ -96,22 +79,6 @@ def get_power_spectral_density_matrix(observation, mask=None, sensor_dim=-2, sou
     return psd
 
 
-def get_pca_vector_old(target_psd_matrix):
-    """
-    Returns the beamforming vector of a PCA beamformer.
-
-    :param target_psd_matrix: Target PSD matrix
-        with shape (bins, sensors, sensors)
-    :return: Set of beamforming vectors with shape (bins, sensors)
-    """
-    bins, sensors, _ = target_psd_matrix.shape
-    beamforming_vector = np.empty((bins, sensors), dtype=np.complex)
-    for f in range(bins):
-        eigenvals, eigenvecs = eigh(target_psd_matrix[f, :, :])
-        beamforming_vector[f, :] = eigenvecs[:, np.argmax(eigenvals)]
-    return beamforming_vector
-
-
 def get_pca_vector(target_psd_matrix):
     """
     Returns the beamforming vector of a PCA beamformer.
@@ -131,7 +98,9 @@ def get_pca_vector(target_psd_matrix):
     # Find max eigenvals
     vals = np.argmax(eigenvals, axis=-1)
     # Select eigenvec for max eigenval
-    beamforming_vector = np.array([eigenvecs[i, :, vals[i]] for i in range(eigenvals.shape[0])])
+    beamforming_vector = np.array(
+        [eigenvecs[i, :, vals[i]] for i in range(eigenvals.shape[0])]
+    )
     # Reconstruct original shape
     beamforming_vector = np.reshape(beamforming_vector, shape[:-1])
 
@@ -156,41 +125,14 @@ def get_mvdr_vector(atf_vector, noise_psd_matrix):
         noise_psd_matrix = np.expand_dims(noise_psd_matrix, axis=0)
 
     # Make sure matrix is hermitian
-    noise_psd_matrix = 0.5 * (noise_psd_matrix + np.conj(noise_psd_matrix.swapaxes(-1, -2)))
+    noise_psd_matrix = 0.5 * (
+        noise_psd_matrix + np.conj(noise_psd_matrix.swapaxes(-1, -2))
+    )
 
     numerator = solve(noise_psd_matrix, atf_vector)
     denominator = np.einsum('...d,...d->...', atf_vector.conj(), numerator)
     beamforming_vector = numerator / np.expand_dims(denominator, axis=-1)
 
-    return beamforming_vector
-
-
-def get_mvdr_vector_old(atf_vector, noise_psd_matrix):
-    """
-    Returns the MVDR beamforming vector.
-
-    :param atf_vector: Acoustic transfer function vector
-        with shape (bins, sensors)
-    :param noise_psd_matrix: Noise PSD matrix
-        with shape (bins, sensors, sensors)
-    :return: Set of beamforming vectors with shape (bins, sensors)
-    """
-
-    if atf_vector.ndim == 1:
-        atf_vector = atf_vector[np.newaxis, :]
-    if noise_psd_matrix.ndim == 2:
-        noise_psd_matrix = noise_psd_matrix[np.newaxis, :, :]
-
-    # Make sure matrix is hermitian
-    noise_psd_matrix = 1 / 2 * (
-        noise_psd_matrix + noise_psd_matrix.transpose(0, 2, 1).conj())
-
-    bins, sensors = atf_vector.shape
-    beamforming_vector = np.empty((bins, sensors), dtype=np.complex)
-    for f in range(bins):
-        numerator = solve(noise_psd_matrix[f, :, :], atf_vector[f, :])
-        denominator = np.dot(atf_vector[f, :].conj(), numerator)
-        beamforming_vector[f, :] = numerator / denominator
     return beamforming_vector
 
 
@@ -204,48 +146,29 @@ def get_gev_vector(target_psd_matrix, noise_psd_matrix):
         with shape (bins, sensors, sensors)
     :return: Set of beamforming vectors with shape (bins, sensors)
     """
-    bins, sensors, _ = target_psd_matrix.shape
+
+    assert target_psd_matrix.shape == noise_psd_matrix.shape
+
+    original_shape = target_psd_matrix.shape
+    sensors = original_shape[-1]
+    target_psd_matrix = target_psd_matrix.reshape((-1, sensors, sensors))
+    noise_psd_matrix = target_psd_matrix.reshape((-1, sensors, sensors))
+
+    bins = target_psd_matrix.shape[0]
     beamforming_vector = np.empty((bins, sensors), dtype=np.complex)
+
     for f in range(bins):
         try:
-            eigenvals, eigenvecs = eigh(target_psd_matrix[f, :, :],
-                                        noise_psd_matrix[f, :, :])
+            eigenvals, eigenvecs = eigh(
+                target_psd_matrix[f, :, :], noise_psd_matrix[f, :, :]
+            )
         except np.linalg.LinAlgError:
-            eigenvals, eigenvecs = eig(target_psd_matrix[f, :, :],
-                                       noise_psd_matrix[f, :, :])
+            eigenvals, eigenvecs = eig(
+                target_psd_matrix[f, :, :], noise_psd_matrix[f, :, :]
+            )
         beamforming_vector[f, :] = eigenvecs[:, np.argmax(eigenvals)]
-    return beamforming_vector
 
-
-def get_lcmv_vector_old(atf_vectors, response_vector, noise_psd_matrix):
-    """
-
-    :param atf_vectors: Acoustic transfer function vectors for
-        each source with shape (bins, targets, sensors)
-    :param response_vector: Defines, which sources you are interested in.
-        Set it to [1, 0, ..., 0], if you are interested in the first speaker.
-        It has the shape (targets,)
-    :param noise_psd_matrix: Noise PSD matrix
-        with shape (bins, sensors, sensors)
-    :return: Set of beamforming vectors with shape (bins, sensors)
-    """
-
-    from scipy.linalg import solve as sci_solve
-
-    if atf_vectors.ndim == 2:
-        atf_vectors = atf_vectors[np.newaxis, :, :]
-    if noise_psd_matrix.ndim == 2:
-        noise_psd_matrix = noise_psd_matrix[np.newaxis, :, :]
-
-    bins, targets, sensors = atf_vectors.shape
-    beamforming_vector = np.empty((bins, sensors), dtype=np.complex)
-    for f in range(bins):
-        Phi_inverse_times_H = sci_solve(noise_psd_matrix[f, :, :], atf_vectors[f, :, :].transpose())
-        H_times_Phi_inverse_times_H = np.dot(atf_vectors[f, :, :].conj(), Phi_inverse_times_H)
-        beamforming_vector[f, :] = np.dot(Phi_inverse_times_H,
-                                          solve(H_times_Phi_inverse_times_H, response_vector))
-
-    return beamforming_vector
+    return beamforming_vector.reshape(original_shape[:-1])
 
 
 def get_lcmv_vector(atf_vectors, response_vector, noise_psd_matrix):
@@ -261,10 +184,24 @@ def get_lcmv_vector(atf_vectors, response_vector, noise_psd_matrix):
     :return: Set of beamforming vectors with shape (bins, sensors)
     """
 
-    Phi_inverse_times_H = solve(np.expand_dims(noise_psd_matrix, axis=0), atf_vectors)
-    H_times_Phi_inverse_times_H = np.einsum('k...d,l...d->...kl', atf_vectors.conj(), Phi_inverse_times_H)
-    temp = solve(H_times_Phi_inverse_times_H, np.expand_dims(response_vector, axis=0))
-    beamforming_vector = np.einsum('k...d,...k->...d', Phi_inverse_times_H, temp)
+    Phi_inverse_times_H = solve(
+        np.expand_dims(noise_psd_matrix, axis=0),
+        atf_vectors
+    )
+    H_times_Phi_inverse_times_H = np.einsum(
+        'k...d,l...d->...kl',
+        atf_vectors.conj(),
+        Phi_inverse_times_H
+    )
+    temp = solve(
+        H_times_Phi_inverse_times_H,
+        np.expand_dims(response_vector, axis=0)
+    )
+    beamforming_vector = np.einsum(
+        'k...d,...k->...d',
+        Phi_inverse_times_H,
+        temp
+    )
 
     return beamforming_vector
 
@@ -308,12 +245,9 @@ def gev_wrapper_on_masks(mix, noise_mask=None, target_mask=None,
     if noise_mask is None:
         noise_mask = np.clip(1 - target_mask, 1e-6, 1)
 
-    bins, sensors, frames = mix.shape
-
     target_psd_matrix = get_power_spectral_density_matrix(mix, target_mask)
     noise_psd_matrix = get_power_spectral_density_matrix(mix, noise_mask)
 
-    # Beamforming vector
     W_gev = get_gev_vector(target_psd_matrix, noise_psd_matrix)
 
     if normalization:
@@ -337,11 +271,8 @@ def pca_wrapper_on_masks(mix, noise_mask=None, target_mask=None):
     if target_mask is None:
         target_mask = np.clip(1 - noise_mask, 1e-6, 1)
 
-    bins, sensors, frames = mix.shape
-
     target_psd_matrix = get_power_spectral_density_matrix(mix, target_mask)
 
-    # Beamforming vector
     W_pca = get_pca_vector(target_psd_matrix)
 
     output = apply_beamforming_vector(W_pca, mix)
@@ -349,7 +280,8 @@ def pca_wrapper_on_masks(mix, noise_mask=None, target_mask=None):
     return output.T
 
 
-def pca_mvdr_wrapper_on_masks(mix, noise_mask=None, target_mask=None, regularization=None):
+def pca_mvdr_wrapper_on_masks(mix, noise_mask=None, target_mask=None,
+                              regularization=None):
     if noise_mask is None and target_mask is None:
         raise ValueError('At least one mask needs to be present.')
 
@@ -364,8 +296,6 @@ def pca_mvdr_wrapper_on_masks(mix, noise_mask=None, target_mask=None, regulariza
     if noise_mask is None:
         noise_mask = np.clip(1 - target_mask, 1e-6, 1)
 
-    bins, sensors, frames = mix.shape
-
     target_psd_matrix = get_power_spectral_density_matrix(mix, target_mask)
     noise_psd_matrix = get_power_spectral_density_matrix(mix, noise_mask)
 
@@ -375,10 +305,9 @@ def pca_mvdr_wrapper_on_masks(mix, noise_mask=None, target_mask=None, regulariza
             (noise_psd_matrix.shape[0], 1, 1)
         )
 
-    # Beamforming vector
     W_pca = get_pca_vector(target_psd_matrix)
     W_mvdr = get_mvdr_vector(W_pca, noise_psd_matrix)
 
-    output = apply_beamforming_vector(W_pca, mix)
+    output = apply_beamforming_vector(W_mvdr, mix)
 
     return output.T
