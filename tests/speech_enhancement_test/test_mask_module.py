08ccbc6cb73b64122546f8ec70b921aa7c053505
Christoph <cbj@mail.uni-paderborn.de>
2016-08-02 15:37:46 +0200

    module_mask: add more testcases

--- tests/speech_enhancement_test/test_mask_module.py
+++ tests/speech_enhancement_test/test_mask_module.py
@@ -1,17 +1,182 @@
 import unittest
 import nt.testing as tc
+from nose_parameterized import parameterized, param
 import numpy as np
 from nt.speech_enhancement.mask_estimation import simple_ideal_soft_mask
 from nt.speech_enhancement.mask_module import ideal_binary_mask
 from nt.speech_enhancement.mask_module import wiener_like_mask
+from nt.utils.random_helper import randn
+import nt.testing as tc
+
+from nt.speech_enhancement import mask_module
 
 
-def _random_stft(*shape):
-    return np.random.rand(*shape) + 1j * np.random.rand(*shape)
+def randc128(*shape):
+    return randn(*shape, dtype=np.complex128)
+
+
+class IdealBinaryMaskTest(unittest.TestCase):
+
+    params = [
+        param('default 2D',
+              kwargs={'signal': randc128(2, 3)},
+              out_shape=(2, 3),
+              out_source_axis=0,),
+    ]+[
+        param('sweep source_axis 2D {}'.format(source_axis),
+              kwargs={'signal': randc128(2, 3), 'source_axis': source_axis},
+              out_shape=(2, 3),
+              out_source_axis=source_axis, )
+        for source_axis in range(2)
+    ]+[
+        param('sweep sensor_axis 2D {} {}'.format(source_axis, sensor_axis),
+              kwargs={'signal': randc128(2, 3),
+                      'source_axis': source_axis,
+                      'sensor_axis': sensor_axis},
+              out_shape=out_shape,
+              out_source_axis=0, )
+        for out_shape, source_axis in zip([(2,), (3,)], range(2))
+        for sensor_axis in range(2) if source_axis != sensor_axis
+    ]+[
+        param('sweep source_axis 3D',
+              kwargs={'signal': randc128(2, 3, 4), 'source_axis': source_axis},
+              out_shape=(2, 3, 4),
+              out_source_axis=source_axis, )
+        for source_axis in range(3)
+    ]+[
+        param('sweep sensor_axis 3D {}, {}'.format(source_axis, sensor_axis),
+              kwargs={'signal': randc128(2, 3, 4),
+                      'source_axis': source_axis,
+                      'sensor_axis': sensor_axis},
+              out_shape=out_shape,
+              out_source_axis=source_axis if sensor_axis > source_axis
+                                          else source_axis - 1, )
+        for source_axis in range(3)
+        for out_shape, sensor_axis in zip([(3, 4), (2, 4), (2, 3)], range(3))
+        if source_axis != sensor_axis
+    ]
+
+    pass_binary_result = False
+    pass_greater_equal_zero = False
+    pass_smaler_equal_one = False
+    pass_sum_to_one = False
+    sensor_axis_allowed = True
+
+    def setUp(self):
+        self.mask_calculator = mask_module.ideal_binary_mask
+
+    @parameterized.expand(params)
+    def test_shape(self, name, kwargs, out_shape, out_source_axis):
+        if not self.sensor_axis_allowed:
+            if len(kwargs) > 2:
+                with tc.assert_raises(AssertionError):
+                    self.mask_calculator(**kwargs)
+                return
+        out = self.mask_calculator(**kwargs)
+        np.testing.assert_array_equal(out.shape, out_shape)
+
+    @parameterized.expand(params)
+    def test_binary_result(self, name, kwargs, out_shape, out_source_axis):
+        if self.pass_binary_result == True:
+            return
+        if not self.sensor_axis_allowed:
+            if len(kwargs) > 2:
+                with tc.assert_raises(AssertionError):
+                    self.mask_calculator(**kwargs)
+                return
+        print(self.pass_binary_result)
+        out = self.mask_calculator(**kwargs)
+        np.testing.assert_array_equal(np.unique(out), [0, 1])
+
+    @parameterized.expand(params)
+    def test_greater_equal_zero(self, name, kwargs, out_shape, out_source_axis):
+        if self.pass_greater_equal_zero:
+            return
+        if not self.sensor_axis_allowed:
+            if len(kwargs) > 2:
+                with tc.assert_raises(AssertionError):
+                    self.mask_calculator(**kwargs)
+                return
+        out = self.mask_calculator(**kwargs)
+        tc.assert_array_greater_equal(np.min(out), 0)
+
+    @parameterized.expand(params)
+    def test_smaler_equal_one(self, name, kwargs, out_shape, out_source_axis):
+        if self.pass_smaler_equal_one:
+            return
+        if not self.sensor_axis_allowed:
+            if len(kwargs) > 2:
+                with tc.assert_raises(AssertionError):
+                    self.mask_calculator(**kwargs)
+                return
+        out = self.mask_calculator(**kwargs)
+        tc.assert_array_less_equal(np.max(out), 1)
+
+    @parameterized.expand(params)
+    def test_sum_to_one(self, name, kwargs, out_shape, out_source_axis):
+        if self.pass_sum_to_one:
+            return
+        if not self.sensor_axis_allowed:
+            if len(kwargs) > 2:
+                with tc.assert_raises(AssertionError):
+                    self.mask_calculator(**kwargs)
+                return
+        out = self.mask_calculator(**kwargs)
+        np.testing.assert_allclose(np.sum(out, out_source_axis), 1)
+
+
+class WienerLikeMaskTestInherint(IdealBinaryMaskTest):
+    def setUp(self):
+        self.mask_calculator = mask_module.wiener_like_mask
+        self.pass_binary_result = True
+
+
+class IdealAmplitudeMaskTestInherint(IdealBinaryMaskTest):
+
+    pass_binary_result = True
+    pass_smaler_equal_one = True
+    pass_sum_to_one = True
+    sensor_axis_allowed = False
+
+    def setUp(self):
+        self.mask_calculator = mask_module.ideal_amplitude_mask
+
+
+class IdealRatioMaskTestInherint(IdealBinaryMaskTest):
+
+    pass_binary_result = True
+    sensor_axis_allowed = False
+
+    def setUp(self):
+        self.mask_calculator = mask_module.ideal_ratio_mask
+
+
+class IdealComplexMaskTestInherint(IdealBinaryMaskTest):
+
+    pass_binary_result = True
+    pass_smaler_equal_one = True
+    pass_greater_equal_zero = True
+    # pass_sum_to_one = True
+    sensor_axis_allowed = False
+
+    def setUp(self):
+        self.mask_calculator = mask_module.ideal_complex_mask
+
+
+class PhaseSensitiveMaskTestInherint(IdealBinaryMaskTest):
+
+    pass_binary_result = True
+    pass_smaler_equal_one = True
+    pass_greater_equal_zero = True
+    # pass_sum_to_one = True
+    sensor_axis_allowed = False
+
+    def setUp(self):
+        self.mask_calculator = mask_module.phase_sensitive_mask
 
 
 F, T, D, K = 51, 31, 6, 2
-X_all = _random_stft(F, T, D, K)
+X_all = randc128(F, T, D, K)
 X, N = (X_all[:, :, :, 0], X_all[:, :, :, 1])
 
 
@@ -44,7 +209,7 @@ class SimpleIdealSoftMaskTests(unittest.TestCase):
 
 class TestIdealBinaryMask(unittest.TestCase):
     def setUp(self):
-        self.signal = _random_stft(K, D, F, T)
+        self.signal = randc128(K, D, F, T)
 
     def test_without_parameters(self):
         mask = ideal_binary_mask(self.signal)
@@ -70,7 +235,7 @@ class TestIdealBinaryMask(unittest.TestCase):
 
 class TestWienerLikeMask(unittest.TestCase):
     def setUp(self):
-        self.signal = _random_stft(K, D, F, T)
+        self.signal = randc128(K, D, F, T)
 
     def test_without_parameters(self):
         mask = wiener_like_mask(self.signal)
@@ -92,3 +257,9 @@ class TestWienerLikeMask(unittest.TestCase):
         signal = np.asarray([0.5 + 0.5j, 0.5 + 0.5j])
         mask = wiener_like_mask(signal)
         tc.assert_equal(mask, [0.5, 0.5])
+
+if __name__ == '__main__':
+
+    import nose
+    nose.runmodule(argv=[__file__, '-vvs', '-x', '--pdb', '--pdb-failure'],
+                   exit=False)
