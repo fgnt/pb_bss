f705ed4c3dc6e45995724dcbc2914a88024a142b
Lukas Drude <mail@lukas-drude.de>
2016-08-05 18:22:16 +0200

    Added Chime background noise. Removed all decorators. Simplified code and disabled all code, which does not work with new design.

--- nt/speech_enhancement/noise/utils.py
+++ nt/speech_enhancement/noise/utils.py
@@ -17,8 +17,8 @@ def get_snr(X, N):
     :param N: Signal of noise image.
     :return: SNR of time signals or STFT signals in dB.
     """
-    energy_X = numpy.sum(numpy.abs(X * X.conj()))
-    energy_N = numpy.sum(numpy.abs(N * N.conj()))
+    energy_X = numpy.mean(numpy.abs(X)**2)
+    energy_N = numpy.mean(numpy.abs(N)**2)
     return 10 * numpy.log10(energy_X / energy_N)
 
 
