e6a47eb267fc99566338c0daea0bc165f49b4bbb
Christoph <cbj@mail.uni-paderborn.de>
2016-08-22 13:12:00 +0200

    mask functions: use warnings.warn for Pycharm, remove old test

--- nt/speech_enhancement/mask_estimation.py
+++ nt/speech_enhancement/mask_estimation.py
@@ -1,17 +1,29 @@
 import numpy as np
 import nt.speech_enhancement.mask_module
+import warnings
 
 
 def simple_ideal_soft_mask(*ins, feature_dim=-2, source_dim=-1,
                            tuple_output=False):
+    warnings.warn(
+        'This function is gone. '
+        'See "nt.speech_enhancement.mask_module.wiener_like_mask" '
+        'for a replacement.',
+        DeprecationWarning
+    )
     raise NotImplementedError(
         'This function is gone. '
         'See "nt.speech_enhancement.mask_module.wiener_like_mask" '
-        'for a replacement.'
-    )
+        'for a replacement.')
 
 
 def quantile_mask(observations, lorenz_fraction=0.98, weight=0.999):
+    warnings.warn(
+        'This function is gone. '
+        'See "nt.speech_enhancement.mask_module.lorenz_mask" '
+        'for a replacement.',
+        DeprecationWarning
+    )
     raise NotImplementedError(
         'This function is gone. '
         'See "nt.speech_enhancement.mask_module.lorenz_mask" '
@@ -28,6 +40,12 @@ def estimate_IBM(
         low_cut=5,
         high_cut=500
 ):
+    warnings.warn(
+        'This function is gone. '
+        'See "nt.speech_enhancement.mask_module.biased_binary_mask" '
+        'for a replacement.',
+        DeprecationWarning
+    )
     raise NotImplementedError(
         'This function is gone. '
         'See "nt.speech_enhancement.mask_module.biased_binary_mask" '
@@ -42,6 +60,12 @@ def estimate_simple_IBM(
         low_cut=5,
         high_cut=500
 ):
+    warnings.warn(
+        'This function is gone. '
+        'See "nt.speech_enhancement.mask_module.biased_binary_mask" '
+        'for a replacement.',
+        DeprecationWarning
+    )
     raise NotImplementedError(
         'This function is gone. '
         'See "nt.speech_enhancement.mask_module.biased_binary_mask" '
