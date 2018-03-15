2735ea57eb2171e3fb09310abd86c0966b83d127
Christoph <cbj@mail.uni-paderborn.de>
2018-03-15 15:15:35 +0100

    set_snr: - add keepdims - allow non inplace

--- nt/speech_enhancement/noise/utils.py
+++ nt/speech_enhancement/noise/utils.py
@@ -1,19 +1,20 @@
 import numpy as np
 
 
-def get_energy(x, axis=None):
-    return np.sum(np.abs(x * x.conj()), axis=axis)
+def get_energy(x, axis=None, keepdims=False):
+    return np.sum(np.abs(x * x.conj()), axis=axis, keepdims=keepdims)
 
 
-def get_variance_for_zero_mean_signal(X, axis=None):
+def get_variance_for_zero_mean_signal(X, axis=None, keepdims=False):
+    X = np.array(X)
     # Bug fix for https://github.com/numpy/numpy/issues/9679
     if np.iscomplexobj(X):
-        return np.mean(X.real ** 2 + X.imag ** 2, axis=axis)
+        return np.mean(X.real ** 2 + X.imag ** 2, axis=axis, keepdims=keepdims)
     else:
-        return np.mean(X ** 2, axis=axis)
+        return np.mean(X ** 2, axis=axis, keepdims=keepdims)
 
 
-def get_snr(X, N, axis=None):
+def get_snr(X, N, *, axis=None, keepdims=False):
     """
     Return SNR of time signals or STFT signals in dB.
     You can use any input dimension. It will always create the mean SNR of all
@@ -24,13 +25,17 @@ def get_snr(X, N, axis=None):
     :param X: Signal of target image.
     :param N: Signal of noise image.
     :return: SNR of time signals or STFT signals in dB.
+
+
+    >>> get_snr([1, 2, 3], [1, 2, 3])
+    0.0
     """
-    power_X = get_variance_for_zero_mean_signal(X, axis=axis)
-    power_N = get_variance_for_zero_mean_signal(N, axis=axis)
+    power_X = get_variance_for_zero_mean_signal(X, axis=axis, keepdims=keepdims)
+    power_N = get_variance_for_zero_mean_signal(N, axis=axis, keepdims=keepdims)
     return 10 * np.log10(power_X / power_N)
 
 
-def set_snr(X, N, snr, current_snr=None, *, axis=None):
+def set_snr(X, N, snr, current_snr=None, *, axis, inplace=True):
     """
     Set the SNR of two input images by rescaling the noise signal in place.
 
@@ -48,6 +53,12 @@ def set_snr(X, N, snr, current_snr=None, *, axis=None):
     """
 
     if current_snr is None:
-        current_snr = get_snr(X, N, axis=axis)
+        current_snr = get_snr(X, N, axis=axis, keepdims=True)
 
-    N *= 10 ** (-(snr - current_snr) / 20)
+    factor = 10 ** (-(snr - current_snr) / 20)
+
+    if inplace:
+        N *= factor
+    else:
+        return X, N * factor
+
