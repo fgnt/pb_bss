5aba157215b045b7e45d85923bd86b3979f8773e
Christoph <cbj@mail.uni-paderborn.de>
2018-05-02 15:12:37 +0200

    ComplexAngularCentralGaussianMixtureModel

--- dc_integration/distribution/cacgmm.py
+++ dc_integration/distribution/cacgmm.py
@@ -4,14 +4,19 @@ import numpy as np
 from .complex_angular_central_gaussian import (
     ComplexAngularCentralGaussianParameters,
 )
+from .util import (
+    _unit_norm,
+)
 
 
 @dataclass
 class ComplexAngularCentralGaussianMixtureModelParameters:
-    complex_angular_central_gaussian: ComplexAngularCentralGaussianParameters \
+    cacg: ComplexAngularCentralGaussianParameters \
         = field(default_factory=ComplexAngularCentralGaussianParameters)
     mixture_weight: np.array = None
     affiliation: np.array = None
+
+    eps: float = 1e-10
 
     def _predict(self, Y):
         """Predict class affiliation posteriors from given model.
@@ -22,26 +27,37 @@ class ComplexAngularCentralGaussianMixtureModelParameters:
             with the same shape.
         """
         F, T, D = Y.shape
-        K = self.pi.shape[-1]
+        K = self.mixture_weight.shape[-1]
         D = Y.shape[-1]
 
         quadratic_form = np.abs(
-            np.einsum('...td,...kde,...te->...kt', Y.conj(), self.precision, Y)
+            np.einsum(
+                '...td,...kde,...te->...kt',
+                Y.conj(),
+                self.cacg.precision,
+                Y,
+            )
         ) + self.eps
         assert quadratic_form.shape == (F, K, T), quadratic_form.shape
 
-        affiliations = - D * np.log(quadratic_form)
-        affiliations -= np.log(self.determinant)[..., None]
-        affiliations = np.exp(affiliations)
-        affiliations *= self.pi[..., None]
-        affiliations /= np.sum(affiliations, axis=-2, keepdims=True) + self.eps
+        affiliation = - D * np.log(quadratic_form)
+        affiliation -= np.log(self.cacg.determinant)[..., None]
+        affiliation = np.exp(affiliation)
+        affiliation *= self.mixture_weight[..., None]
 
-        affiliations = np.clip(affiliations, self.eps, 1 - self.eps)
+        # ToDo: Figure out if
+        # >>> affiliations /= np.sum(affiliations, axis=-2, keepdims=True) + self.eps
+        # >>> affiliations = np.clip(affiliations, self.eps, 1 - self.eps)
+        # or
+        # >>> affiliations = np.clip(affiliations, self.eps, 1 - self.eps)
+        # >>> affiliations /= np.sum(affiliations, axis=-2, keepdims=True) + self.eps
+        # is better
+        affiliation /= np.sum(affiliation, axis=-2, keepdims=True) + self.eps
+        affiliation = np.clip(affiliation, self.eps, 1 - self.eps)
 
-        if self.visual_debug:
-            _plot_affiliations(affiliations)
-
-        return affiliations, quadratic_form
+        # if self.visual_debug:
+        #     _plot_affiliations(affiliations)
+        return affiliation, quadratic_form
 
     def predict(self, Y):
         """Predict class affiliation posteriors from given model.
@@ -57,13 +73,14 @@ class ComplexAngularCentralGaussianMixtureModel:
     """Ito 2016."""
     unit_norm = staticmethod(_unit_norm)
 
-    def __init__(self, eps=1e-10, visual_debug=False):
+    def __init__(self, eps=1e-10, visual_debug=False, pbar=False):
         self.eps = eps
         self.visual_debug = visual_debug
-        self.covariance = np.empty((), dtype=np.complex)
-        self.precision = np.empty((), dtype=np.complex)
-        self.determinant = np.empty((), dtype=np.float)
-        self.pi = np.empty((), dtype=np.float)
+        # self.covariance = np.empty((), dtype=np.complex)
+        # self.precision = np.empty((), dtype=np.complex)
+        # self.determinant = np.empty((), dtype=np.float)
+        # self.pi = np.empty((), dtype=np.float)
+        self.pbar = pbar
 
     def fit(
             self, Y, initialization, iterations=100,
@@ -76,75 +93,101 @@ class ComplexAngularCentralGaussianMixtureModel:
             iterations:
             initialization: Shape (..., K, T).
         """
-        F, T, D = Y.shape
+
+        *independent, T, D = Y.shape
+        independent = tuple(independent)
         K = initialization.shape[-2]
-        Y_for_psd = np.copy(np.swapaxes(Y, -2, -1), 'C')[..., None, :, :]
-        Y_for_pdf = np.copy(Y, 'C')
-        D = Y_for_pdf.shape[-1]
+        assert K < 20, (K, 'Sure?')
+        assert D < 20, (D, 'Sure?')
+        assert initialization.shape[-1] == T, (initialization.shape, T)
+        assert initialization.shape[:-2] == independent, (initialization.shape, independent)
 
-        affiliations = np.copy(initialization)
-        quadratic_form = np.ones_like(affiliations)
+        Y = _unit_norm(
+            Y,
+            axis=-1,
+            eps=1e-10,
+            eps_style='where'
+        )
 
-        for i in range(iterations):
+        Y_for_psd = np.ascontiguousarray(np.swapaxes(Y, -2, -1))
+        # Y_for_psd: Shape (..., T, K)
+        Y_for_pdf = np.ascontiguousarray(Y)
+
+        params = ComplexAngularCentralGaussianMixtureModelParameters(
+            eps=self.eps
+        )
+
+        params.affiliation = np.copy(initialization)  # Shape (..., K, T)
+        quadratic_form = np.ones_like(params.affiliation)  # Shape (..., K, T)
+
+        if self.pbar:
+            import tqdm
+            range_iterations = tqdm.tqdm(range(iterations), 'cACGMM Iteration')
+        else:
+            range_iterations = range(iterations)
+
+        for i in range_iterations:
             # E step
             if i > 0:
                 # Equation 12
-                affiliations, quadratic_form = self._predict(Y_for_pdf)
+                params.affiliation, quadratic_form = params._predict(Y_for_pdf)
 
-            self.pi = np.mean(affiliations, axis=-1)
-            assert self.pi.shape == (F, K), self.pi.shape
+            params.mixture_weight = np.mean(params.affiliation, axis=-1)
+            assert params.mixture_weight.shape == (*independent, K), params.mixture_weight.shape
 
-            mask = affiliations[..., None, :]
-            assert mask.shape == (F, K, 1, T), mask.shape
-            self.covariance = D * np.einsum(
-                '...dt,...et->...de',
-                (mask / quadratic_form[..., None, :]) * Y_for_psd,
+            mask = params.affiliation[..., None, :]
+            assert mask.shape == (*independent, K, 1, T), mask.shape
+            params.cacg.covariance = D * np.einsum(
+                '...kt,...dt,...et->...kde',
+                (params.affiliation / quadratic_form),
+                Y_for_psd,
                 Y_for_psd.conj()
             )
             normalization = np.sum(mask, axis=-1, keepdims=True)
-            self.covariance /= normalization
-            assert self.covariance.shape == (F, K, D, D), self.covariance.shape
+            params.cacg.covariance /= normalization
+            assert params.cacg.covariance.shape == (*independent, K, D, D), params.cacg.covariance.shape
 
             if hermitize:
-                self.covariance = (
-                                      self.covariance
-                                      + np.swapaxes(self.covariance.conj(), -1,
-                                                    -2)
-                                  ) / 2
+                params.cacg.covariance = \
+                    (
+                            params.cacg.covariance
+                            + np.swapaxes(params.cacg.covariance.conj(), -1, -2)
+                    ) / 2
 
             if trace_norm:
-                self.covariance /= np.einsum(
-                    '...dd', self.covariance
+                params.cacg.covariance /= np.einsum(
+                    '...dd', params.cacg.covariance
                 )[..., None, None]
 
-            eigenvals, eigenvecs = np.linalg.eigh(self.covariance)
+            eigenvals, eigenvecs = np.linalg.eigh(params.cacg.covariance)
             eigenvals = eigenvals.real
             eigenvals = np.maximum(
                 eigenvals,
                 np.max(eigenvals, axis=-1, keepdims=True) * eigenvalue_floor
             )
-            diagonal = np.einsum('de,fkd->fkde', np.eye(D), eigenvals)
-            self.covariance = np.einsum(
-                'fkwx,fkxy,fkzy->fkwz', eigenvecs, diagonal, eigenvecs.conj()
+            diagonal = np.einsum('de,...kd->...kde', np.eye(D), eigenvals)
+            params.cacg.covariance = np.einsum(
+                '...kwx,...kxy,...kzy->...kwz', eigenvecs, diagonal, eigenvecs.conj()
             )
-            self.determinant = np.prod(eigenvals, axis=-1)
-            inverse_diagonal = np.einsum('de,fkd->fkde', np.eye(D),
+            params.cacg.determinant = np.prod(eigenvals, axis=-1)
+            inverse_diagonal = np.einsum('de,...kd->...kde', np.eye(D),
                                          1 / eigenvals)
-            self.precision = np.einsum(
-                'fkwx,fkxy,fkzy->fkwz', eigenvecs, inverse_diagonal,
+            params.cacg.precision = np.einsum(
+                '...kwx,...kxy,...kzy->...kwz', eigenvecs, inverse_diagonal,
                 eigenvecs.conj()
             )
+        return params
 
-            if self.visual_debug:
-                with context_manager(figure_size=(24, 3)):
-                    plt.plot(np.log10(
-                        np.max(eigenvals, axis=-1)
-                        / np.min(eigenvals, axis=-1)
-                    ))
-                    plt.xlabel('frequency bin')
-                    plt.ylabel('eigenvalue spread')
-                    plt.show()
-                with context_manager(figure_size=(24, 3)):
-                    plt.plot(self.pi)
-                    plt.show()
+            # if self.visual_debug:
+            #     with context_manager(figure_size=(24, 3)):
+            #         plt.plot(np.log10(
+            #             np.max(eigenvals, axis=-1)
+            #             / np.min(eigenvals, axis=-1)
+            #         ))
+            #         plt.xlabel('frequency bin')
+            #         plt.ylabel('eigenvalue spread')
+            #         plt.show()
+            #     with context_manager(figure_size=(24, 3)):
+            #         plt.plot(self.pi)
+            #         plt.show()
 
