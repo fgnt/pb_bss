ea866da3c63d1ec8e4fc05a21be79b7b2184b6e6
Christoph <cbj@mail.uni-paderborn.de>
2018-05-02 17:41:52 +0200

    ComplexAngularCentralGaussianMixtureModelParameters: add souce_activity_mask

--- dc_integration/distribution/cacgmm.py
+++ dc_integration/distribution/cacgmm.py
@@ -19,15 +19,16 @@ class ComplexAngularCentralGaussianMixtureModelParameters:
 
     eps: float = 1e-10
 
-    def _predict(self, Y):
+    def _predict(self, Y, souce_activity_mask=None):
         """Predict class affiliation posteriors from given model.
 
         Args:
             Y: Normalized observations with shape (..., T, D).
+            souce_activity_mask: shape (..., K, T)
         Returns: Affiliations with shape (..., K, T) and quadratic format
             with the same shape.
         """
-        F, T, D = Y.shape
+        *independent, T, D = Y.shape
         K = self.mixture_weight.shape[-1]
         D = Y.shape[-1]
 
@@ -39,21 +40,25 @@ class ComplexAngularCentralGaussianMixtureModelParameters:
                 Y,
             )
         ) + self.eps
-        assert quadratic_form.shape == (F, K, T), quadratic_form.shape
+        assert quadratic_form.shape == (*independent, K, T), quadratic_form.shape
 
         affiliation = - D * np.log(quadratic_form)
         affiliation -= np.log(self.cacg.determinant)[..., None]
         affiliation = np.exp(affiliation)
         affiliation *= self.mixture_weight[..., None]
 
+        if souce_activity_mask is not None:
+            affiliation *= souce_activity_mask
+
         # ToDo: Figure out if
-        # >>> affiliations /= np.sum(affiliations, axis=-2, keepdims=True) + self.eps
+        #  >>> affiliations /= np.sum(affiliations, axis=-2, keepdims=True) + self.eps
         # >>> affiliations = np.clip(affiliations, self.eps, 1 - self.eps)
         # or
         # >>> affiliations = np.clip(affiliations, self.eps, 1 - self.eps)
         # >>> affiliations /= np.sum(affiliations, axis=-2, keepdims=True) + self.eps
         # is better
-        affiliation /= np.sum(affiliation, axis=-2, keepdims=True) + self.eps
+
+        affiliation / np.sum(affiliation, axis=-2, keepdims=True) + self.eps
         affiliation = np.clip(affiliation, self.eps, 1 - self.eps)
 
         # if self.visual_debug:
@@ -80,15 +85,25 @@ class ComplexAngularCentralGaussianMixtureModel:
         self.pbar = pbar
 
     def fit(
-            self, Y, initialization, iterations=100,
-            hermitize=True, trace_norm=True, eigenvalue_floor=1e-10
+            self,
+            Y,
+            initialization,
+            souce_activity_mask=None,
+            iterations=100,
+            hermitize=True,
+            trace_norm=True,
+            eigenvalue_floor=1e-10,
     ) -> ComplexAngularCentralGaussianMixtureModelParameters:
         """Fit a cACGMM.
 
         Args:
             Y: Normalized observations with shape (..., T, D).
-            iterations:
             initialization: Shape (..., K, T).
+            souce_activity_mask: Shape (..., K, T)
+                A binary mask that indicate is a source is active or not at a
+                time point. Example about a voice activity detection determines
+                sections where only noise is active, then this mask can set the
+                activity of all speakers at that time point to zero.
         """
 
         *independent, T, D = Y.shape
@@ -118,6 +133,10 @@ class ComplexAngularCentralGaussianMixtureModel:
         params.affiliation = np.copy(initialization)  # Shape (..., K, T)
         quadratic_form = np.ones_like(params.affiliation)  # Shape (..., K, T)
 
+        if souce_activity_mask is not None:
+            assert souce_activity_mask.dtype == np.bool, souce_activity_mask.dtype
+            assert souce_activity_mask.shape == params.affiliation.shape, (souce_activity_mask.shape, params.affiliation.shape)
+
         if self.pbar:
             import tqdm
             range_iterations = tqdm.tqdm(range(iterations), 'cACGMM Iteration')
@@ -128,7 +147,9 @@ class ComplexAngularCentralGaussianMixtureModel:
             # E step
             if i > 0:
                 # Equation 12
-                params.affiliation, quadratic_form = params._predict(Y_for_pdf)
+                params.affiliation, quadratic_form = params._predict(
+                    Y_for_pdf, souce_activity_mask=souce_activity_mask
+                )
 
             params.mixture_weight = np.mean(params.affiliation, axis=-1)
             assert params.mixture_weight.shape == (*independent, K), params.mixture_weight.shape
@@ -155,4 +176,3 @@ class ComplexAngularCentralGaussianMixtureModel:
             #     with context_manager(figure_size=(24, 3)):
             #         plt.plot(self.pi)
             #         plt.show()
-
