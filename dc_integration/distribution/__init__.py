696fe97ff239c0d48294c46e431c7ff1d8960b09
Christoph <cbj@mail.uni-paderborn.de>
2018-05-07 17:17:20 +0200

    fill __init__.py of distribution folder

---
+++ dc_integration/distribution/__init__.py
@@ -0,0 +1,16 @@
+from .complex_angular_central_gaussian import (
+    ComplexAngularCentralGaussian,
+    ComplexAngularCentralGaussianParameters,
+)
+from .cacgmm import (
+    ComplexAngularCentralGaussianMixtureModel,
+    ComplexAngularCentralGaussianMixtureModelParameters,
+)
+from .complex_watson import (
+    ComplexWatson,
+    ComplexWatsonParameters,
+)
+from .cwmm import (
+    ComplexWatsonMixtureModel,
+    ComplexWatsonMixtureModelParameters,
+)
