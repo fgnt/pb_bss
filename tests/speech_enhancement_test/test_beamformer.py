dacf1bab40ffcfdaec00014bb7a9fa996d2d2535
Christoph <cbj@mail.uni-paderborn.de>
2018-08-09 12:39:17 +0200

    update get_mvdr_vector_souden to use a SNR estimate

--- tests/speech_enhancement_test/test_beamformer.py
+++ tests/speech_enhancement_test/test_beamformer.py
@@ -1,7 +1,7 @@
 import unittest
 
 import numpy as np
-
+import functools
 import nt.testing as tc
 from nt.speech_enhancement.beamformer import get_gev_vector, \
     _get_gev_vector, _cythonized_eig, eig
@@ -125,3 +125,200 @@ class TestCythonizedEig(unittest.TestCase):
 
         # assume speedup is bigger than 5
         tc.assert_array_greater(elapsed_time_python / elapsed_time_cython1, 4)
+
+
+from nt.speech_enhancement.beamformer import get_mvdr_vector_souden
+
+
+class TestMvdrSouden(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        obs = np.array([[0, 0, 1], [0, 0.1, 1], [0.1, 0, 1]])
+        self.PhiXX = obs.T.conj() @ obs
+        np.testing.assert_allclose(self.PhiXX, [
+            [0.01, 0., 0.1],
+            [0., 0.01, 0.1],
+            [0.1, 0.1, 3.],
+        ])
+        self.PhiNN = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+    def get_w_well_behaviour(self):
+        w, = get_mvdr_vector_souden(
+            self.PhiXX[None, ...],
+            self.PhiNN[None, ...],
+        )
+        return w
+
+    def test_well_behaviour(self):
+        w = self.get_w_well_behaviour()
+        assert repr(w) == 'array([0.03311258, 0.03311258, 0.99337748])', repr(w)
+        w3 = get_mvdr_vector_souden([self.PhiXX] * 3, [self.PhiNN] * 3)
+        np.testing.assert_allclose([w] * 3, w3)
+
+    def test_difficulties(
+            self,
+    ):
+        get_beamformer = get_mvdr_vector_souden
+
+        for args in [
+            (
+                self.PhiXX[None, ...] * 0,
+                self.PhiNN[None, ...],
+            ),
+            (
+                self.PhiXX[None, ...],
+                self.PhiNN[None, ...] * 0,
+            ),
+            (
+                self.PhiXX[None, ...] * 0,
+                self.PhiNN[None, ...] * 0,
+            ),
+        ]:
+            w = get_beamformer(*args)
+            assert repr(w) == 'array([[0., 0., 0.]])', repr(w)
+
+        for args in [
+            (
+                self.PhiXX[None, ...] * np.inf,
+                self.PhiNN[None, ...],
+            ),
+            (
+                self.PhiXX[None, ...],
+                self.PhiNN[None, ...] * np.inf,
+            ),
+            (
+                self.PhiXX[None, ...] * np.inf,
+                self.PhiNN[None, ...] * np.inf,
+            ),
+        ]:
+            with tc.assert_raises(AssertionError):
+                get_beamformer(*args)
+
+    def test_difficulties_without_eps_single(self):
+        def get_beamformer(A, B):
+            return get_mvdr_vector_souden(
+                A, B, eps=0
+            )
+        for args in [
+            (
+                self.PhiXX[None, ...] * 0,
+                self.PhiNN[None, ...],
+            ),
+            (
+                self.PhiXX[None, ...],
+                self.PhiNN[None, ...] * 0,
+            ),
+            (
+                self.PhiXX[None, ...] * 0,
+                self.PhiNN[None, ...] * 0,
+            ),
+            (
+                self.PhiXX[None, ...] * np.inf,
+                self.PhiNN[None, ...],
+            ),
+            (
+                self.PhiXX[None, ...],
+                self.PhiNN[None, ...] * np.inf,
+            ),
+            (
+                self.PhiXX[None, ...] * np.inf,
+                self.PhiNN[None, ...] * np.inf,
+            ),
+        ]:
+            with tc.assert_raises(AssertionError):
+                get_beamformer(*args)
+
+    def test_difficulties_eps_multi(self):
+        """
+        Broken frequencies only damage total result, when they have inf
+        values. Zeros are ok for target_psd_matrix and noise_psd_matrix.
+        """
+        well_w = self.get_w_well_behaviour()
+
+        def get_beamformer(A, B):
+            return get_mvdr_vector_souden(
+                A, B,
+                return_ref_channel=True
+            )
+
+        for args in [
+            (
+                [self.PhiXX * 0, self.PhiXX],
+                [self.PhiNN, self.PhiNN],
+            ),
+            (
+                [self.PhiXX, self.PhiXX],
+                [self.PhiNN * 0, self.PhiNN],
+            ),
+            (
+                [self.PhiXX * 0, self.PhiXX],
+                [self.PhiNN * 0, self.PhiNN],
+            ),
+        ]:
+            w, ref_channel = get_beamformer(*args)
+            assert ref_channel == 2, ref_channel
+            np.testing.assert_allclose(
+                w,
+                np.array([[0., 0., 0.], well_w])
+            )
+
+        for args in [
+            (
+                [self.PhiXX * np.inf, self.PhiXX],
+                [self.PhiNN, self.PhiNN],
+            ),
+            (
+                [self.PhiXX, self.PhiXX],
+                [self.PhiNN * np.inf, self.PhiNN],
+            ),
+            (
+                [self.PhiXX * np.inf, self.PhiXX],
+                [self.PhiNN * np.inf, self.PhiNN],
+            ),
+        ]:
+            with tc.assert_raises(AssertionError):
+                get_beamformer(*args)
+
+    def test_difficulties_without_eps_multi(self):
+        """
+        When eps is set to zero, any broken frequency will damage the
+        ref_channel estimation. Independent if target_psd_matrix or
+        noise_psd_matrix is zero or inf.
+        """
+
+        def get_beamformer(A, B):
+            return get_mvdr_vector_souden(
+                A, B,
+                eps=0,
+                return_ref_channel=True
+            )
+
+        for args in [
+            (
+                [self.PhiXX * 0, self.PhiXX],
+                [self.PhiNN, self.PhiNN],
+            ),
+            (
+                [self.PhiXX, self.PhiXX],
+                [self.PhiNN * 0, self.PhiNN],
+            ),
+            (
+                [self.PhiXX * 0, self.PhiXX],
+                [self.PhiNN * 0, self.PhiNN],
+            ),
+            (
+                [self.PhiXX * np.inf, self.PhiXX],
+                [self.PhiNN, self.PhiNN],
+            ),
+            (
+                [self.PhiXX, self.PhiXX],
+                [self.PhiNN * np.inf, self.PhiNN],
+            ),
+            (
+                [self.PhiXX * np.inf, self.PhiXX],
+                [self.PhiNN * np.inf, self.PhiNN],
+            ),
+        ]:
+            with tc.assert_raises(AssertionError):
+                get_beamformer(*args)
