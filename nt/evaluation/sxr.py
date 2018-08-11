b6ee3d5b67fcb6903156a2b59f4a3832323adf27
Lukas Drude <mail@lukas-drude.de>
2018-08-11 17:44:16 +0200

    output_sxr was wrong when using more outputs than true sources.

--- nt/evaluation/sxr.py
+++ nt/evaluation/sxr.py
@@ -1,6 +1,7 @@
-import numpy
+import numpy as np
 import itertools
 from nt.speech_enhancement.noise import get_energy, get_snr
+from scipy.special import perm
 
 
 __all__ = ['get_snr', 'input_sxr', 'output_sxr']
@@ -13,8 +14,8 @@ def _sxr(S, X):
     :param X: X power
     :return: SXR
     """
-    with numpy.errstate(divide='ignore'):
-        result = 10 * numpy.log10(S/X)
+    with np.errstate(divide='ignore'):
+        result = 10 * np.log10(S/X)
     return result
 
 
@@ -29,9 +30,9 @@ def input_sxr(images, noise, average_sources=True, *, return_dict=False):
     room impulse response and before mixing.
 
     :param images: Array of unmixed but reverberated speech in time domain
-    :type images: numpy.ndarray with #Samples x #Sensors x #Speakers
+    :type images: np.ndarray with #Samples x #Sensors x #Speakers
     :param noise: Array of ground truth noise
-    :type noise: numpy.ndarray with #Samples x #Sensors
+    :type noise: np.ndarray with #Samples x #Sensors
     :param average_sources: Logical if SXR is average of speakers
     :type average_sources: bool
     :param return_dict: specifies if the returned value is a list or a dict.
@@ -47,26 +48,26 @@ def input_sxr(images, noise, average_sources=True, *, return_dict=False):
     D = images.shape[1]  # Number of sensors
     K = images.shape[2]  # Number of speakers
 
-    S = numpy.zeros((K, D))  # Signal power
-    I = numpy.zeros((K, D))  # Interference power
-    N = numpy.zeros(D)  # Noise power
+    S = np.zeros((K, D))  # Signal power
+    I = np.zeros((K, D))  # Interference power
+    N = np.zeros(D)  # Noise power
 
     for d in range(D):
         for k in range(K):
             S[k, d] = get_energy(images[:, d, k], axis=0)
-            I[k, d] = numpy.sum(
+            I[k, d] = np.sum(
                 get_energy(images[:, d, [n for n in range(K) if n != k]],
                            axis=0))
         N[d] = get_energy(noise[:, d], axis=0)
 
     if average_sources:
-        S = numpy.mean(S)
-        I = numpy.mean(I)
+        S = np.mean(S)
+        I = np.mean(I)
     else:
-        S = numpy.mean(S, axis=1)
-        I = numpy.mean(I, axis=1)
+        S = np.mean(S, axis=1)
+        I = np.mean(I, axis=1)
 
-    N = numpy.mean(N)
+    N = np.mean(N)
 
     SDR = _sxr(S, I + N)
     SIR = _sxr(S, I)
@@ -100,16 +101,16 @@ def output_sxr(image_contribution, noise_contribution, average_sources=True,
 
     Use the mixed signals to run your algorithm. Save the estimated separation
     parameters (i.e. beamforming vectors, gain functions, ...) and apply the
-    algorithm with the fixed paramters to the clean images and ground truth
+    algorithm with the fixed parameters to the clean images and ground truth
     noise separately. Evaluate the results with this function to obtain
     intrusive SXR measures.
 
-    :param image_contribution:  Put each of the clean images into the
+    :param image_contribution: Put each of the clean images into the
       separation algorithm with fixed parameters. The output of the separation
-      algorithm can now be used as imageContribution.
+      algorithm can now be used as `image_contribution`.
     :type image_contribution: #Samples x #sourceSpeakers x #targetSpeakers
     :param noise_contribution: Put the ground truth noise into the separation
-      algorithm with fixed parameters. The output is noiseContribution.
+      algorithm with fixed parameters. The output is `noise_contribution`.
     :type noise_contribution: #Samples times #targetSpeakers
     :param average_sources: Scalar logical if SXR is average of speakers;
       optional (default: true). If set to true, SXR-values are scalars.
@@ -122,19 +123,16 @@ def output_sxr(image_contribution, noise_contribution, average_sources=True,
     :return SIR: #Sources times 1 vector of Signal to Interference Ratios
     :return SNR: #Sources times 1 vector of Signal to Noise Ratios
    """
-    # TODO: This is not quite the correct way when utterances have different
-    # len
-
     # Assume, that the maximum number of speakers is smaller than 10.
     assert(image_contribution.shape[1] < 10)
     assert(image_contribution.shape[2] < 10)
     assert(noise_contribution.shape[1] < 10)
 
     K_source = image_contribution.shape[1]
-    K_target = image_contribution.shape[2]  # Number of target speaker
+    K_target = image_contribution.shape[2]
 
-    S = numpy.zeros((K_source, K_target))
-    N = numpy.zeros(K_target)
+    S = np.zeros((K_source, K_target))
+    N = np.zeros(K_target)
 
     for k_target in range(K_target):
         for k_source in range(K_source):
@@ -143,35 +141,44 @@ def output_sxr(image_contribution, noise_contribution, average_sources=True,
             )
         N[k_target] = get_energy(noise_contribution[:, k_target], axis=0)
 
-    all_permutations = \
-        numpy.array(list(itertools.permutations(range(K_target))))
+    # We actually do not need to go through all permutations but through
+    # all possible selections (picks) of the output sources to find that pick
+    # which best matches the oracle sources.
+    all_target_selections = np.array(
+        list(itertools.permutations(range(K_target), r=K_source))
+    )
+    assert all_target_selections.shape == (perm(K_target, K_source), K_source), (
+        all_target_selections.shape, perm(K_target, K_source), K_source
+    )
 
-    mutual_power = numpy.zeros(all_permutations.shape[0])
+    mutual_power = np.zeros(all_target_selections.shape[0])
 
-    for p in range(all_permutations.shape[0]):
-        for k_target in range(K_target):
-            mutual_power[p] = mutual_power[p] + \
-                S[all_permutations[p, k_target], k_target]
+    for p in range(all_target_selections.shape[0]):
+        mutual_power[p] = np.sum([
+            S[k_source, all_target_selections[p, k_source]]
+            for k_source in range(K_source)
+        ])
 
-    max_idx = numpy.argmax(mutual_power)
-    permutation = all_permutations[max_idx]
+    max_idx = np.argmax(mutual_power)
+    selection = all_target_selections[max_idx]
 
-    SS = numpy.zeros(K_target)
-    II = numpy.zeros(K_target)
-    for k_target in range(K_target):
-        SS[k_target] = S[k_target, permutation[k_target]]
-        II[k_target] = numpy.sum(
-            S[[n for n in range(K_target) if n != k_target],
-              permutation[k_target]])
+    SS = np.zeros(K_source)
+    II = np.zeros(K_source)
+    for k_source in range(K_source):
+        SS[k_source] = S[k_source, selection[k_source]]
+        II[k_source] = np.sum(
+            np.delete(S[:, selection[k_source]], k_source, axis=0)
+        )
+    NN = N[selection]
 
     if average_sources:
-        SS = numpy.mean(SS)
-        II = numpy.mean(II)
-    N = numpy.mean(N)
+        SS = np.mean(SS)
+        II = np.mean(II)
+        NN = np.mean(NN)
 
-    SDR = _sxr(SS, II + N)
+    SDR = _sxr(SS, II + NN)
     SIR = _sxr(SS, II)
-    SNR = _sxr(SS, N)
+    SNR = _sxr(SS, NN)
 
     if return_dict:
         if return_dict is True:
