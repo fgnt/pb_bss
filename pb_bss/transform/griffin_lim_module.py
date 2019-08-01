e8ae28358e90585e67cbb07fcbc3ee1ce7311d17
Lukas Drude <mail@lukas-drude.de>
2019-08-02 00:01:09 +0200

    Document sources

--- pb_bss/transform/griffin_lim_module.py
+++ pb_bss/transform/griffin_lim_module.py
@@ -6,6 +6,29 @@ from paderbox.speech_enhancement.noise import get_variance_for_zero_mean_signal
 
 
 class GriffinLim:
+    """
+    @article{Griffin1984GriffinLim,
+      title={Signal estimation from modified short-time Fourier transform},
+      author={Griffin, Daniel and Lim, Jae},
+      journal={IEEE Transactions on Acoustics, Speech, and Signal Processing},
+      volume={32},
+      number={2},
+      pages={236--243},
+      year={1984},
+      publisher={IEEE}
+    }
+
+    @article{Gunawan2010MISI,
+      title={Iterative phase estimation for the synthesis of separated sources from single-channel mixtures},
+      author={Gunawan, David and Sen, Deep},
+      journal={IEEE Signal Processing Letters},
+      volume={17},
+      number={5},
+      pages={421--424},
+      year={2010},
+      publisher={IEEE}
+    }
+    """
     def __init__(
             self,
             X,
@@ -29,6 +52,7 @@ class GriffinLim:
             self.x_hat = self.istft(X)
         elif first_guess == 'y':
             K = X.shape[0]
+            # Text just under [Gunawan2010MISI] Equation 5
             self.x_hat = np.repeat(self.y[None, :] / K, K, axis=0)
         else:
             raise ValueError(first_guess)
@@ -62,6 +86,18 @@ class GriffinLim:
 
 
 class MISI(GriffinLim):
+    """
+    @article{Gunawan2010MISI,
+      title={Iterative phase estimation for the synthesis of separated sources from single-channel mixtures},
+      author={Gunawan, David and Sen, Deep},
+      journal={IEEE Signal Processing Letters},
+      volume={17},
+      number={5},
+      pages={421--424},
+      year={2010},
+      publisher={IEEE}
+    }
+    """
     def step(self):
         """
         Do we need power a adjusted version?
@@ -73,8 +109,17 @@ class MISI(GriffinLim):
 
         """
         K = self.X.shape[0]
+
+        # [Gunawan2010MISI] Equation 5
         e = self.y - np.sum(self.x_hat, axis=0)
+
+        # [Gunawan2010MISI] Equation 4
         x_dash_dash = self.x_hat + e / K
+
         self.X_dash_dash = self.stft(x_dash_dash)
+
+        # [Gunawan2010MISI] Equation 3
         self.X_dash = np.abs(self.X) * np.exp(1j * np.angle(self.X_dash_dash))
+
+        # [Gunawan2010MISI] Equation 2
         self.x_hat = self.istft(self.X_dash)
