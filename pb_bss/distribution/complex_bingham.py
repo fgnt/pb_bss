688caf3d327baa1685a4c4f396b0f43d06d4027c
Christoph <cbj@mail.uni-paderborn.de>
2019-08-05 22:02:45 +0200

    WIP: Bingham: add max_concentration and affiliation_eps

--- pb_bss/distribution/complex_bingham.py
+++ pb_bss/distribution/complex_bingham.py
@@ -7,7 +7,7 @@ from scipy.optimize import least_squares
 
 from pb_bss.distribution.utils import _ProbabilisticModel
 from pb_bss.utils import is_broadcast_compatible
-from pb_bss.distribution.complex_bingham_utils import grad_log_norm_symbolic
+from pb_bss.distribution.complex_bingham_utils import grad_log_norm_symbolic, grad_log_norm_symbolic_diff
 
 def normalize_observation(observation):
     """
@@ -80,7 +80,7 @@ class ComplexBingham(_ProbabilisticModel):
     def log_norm(self, remove_duplicate_eigenvalues=True):
         return np.log(self.norm(remove_duplicate_eigenvalues=remove_duplicate_eigenvalues))
 
-    def norm(self, remove_duplicate_eigenvalues=True):
+    def norm(self, remove_duplicate_eigenvalues=True, eps=1e-8):
         """
         >>> model = ComplexBingham(None, [0.8       , 0.92679492, 1.27320508])
         >>> model.covariance_eigenvalues
@@ -135,10 +135,25 @@ class ComplexBingham(_ProbabilisticModel):
         ...     5.15996555e-04, 6.28805516e-04, 1.37554184e-03, 1.53621463e-02,
         ...     3.74437619e-02, 9.44673748e-01])).norm()
         19.0955491592929
+
+
+        >>> values = [-10.00000004, -10.00000003, -10.00000002, -10.00000001, -10., 0.]
+        >>> ComplexBingham(None, np.array(values)).norm(eps=1e-8)
+        8258270290267.509
+        >>> ComplexBingham(None, np.array(values)).norm(eps=1e-7)
+        8258270290267.509
+        >>> ComplexBingham(None, np.array(values)).norm(eps=1e-6)
+        8258270290267.509
+        >>> ComplexBingham(None, np.array(values)).norm(eps=1e-5)
+        8258270290267.509
+
+
         """
         covariance_eigenvalues = self.covariance_eigenvalues
         if remove_duplicate_eigenvalues:
-            _, covariance_eigenvalues = self._remove_duplicate_eigenvalues(covariance_eigenvalues)
+            _, covariance_eigenvalues = self._remove_duplicate_eigenvalues(
+                covariance_eigenvalues, eps=eps
+            )
 
         deltas = covariance_eigenvalues[..., None] - covariance_eigenvalues[..., None, :]
         D = deltas.shape[-1]
@@ -151,11 +166,11 @@ class ComplexBingham(_ProbabilisticModel):
     def _remove_duplicate_eigenvalues(cls, covariance_eigenvalues, eps=1e-8):
         """
         >>> ComplexBingham._remove_duplicate_eigenvalues(np.array([0.5, 0.5]))[-1]
-        array([0.49999999, 0.5       ])
+        array([0.5       , 0.50000001])
 
         Demonstrate the suboptimal behaviour for duplicate eigenvalues.
         >>> ComplexBingham._remove_duplicate_eigenvalues(np.array([0.2, 0.4, 0.4]), eps=0.02)[-1]
-        array([0.192, 0.392, 0.4  ])
+        array([0.2 , 0.4 , 0.42])
 
         This function sorts the eigenvalues
         >>> ComplexBingham._remove_duplicate_eigenvalues(np.array([0.9, 0.1]))
@@ -164,17 +179,21 @@ class ComplexBingham(_ProbabilisticModel):
         (array([2, 1, 0]), array([0.04, 0.06, 0.9 ]))
         >>> ComplexBingham._remove_duplicate_eigenvalues(np.array([0.9, 0.04, 0.06]))
         (array([2, 0, 1]), array([0.04, 0.06, 0.9 ]))
+
+        >>> ComplexBingham._remove_duplicate_eigenvalues(np.array([1, 0.0, 0.0]))
+        (array([2, 0, 1]), array([0.00000000e+00, 1.00000000e-08, 1.00000001e+00]))
         """
         permutation = np.argsort(covariance_eigenvalues, axis=-1, )
         covariance_eigenvalues = np.take_along_axis(covariance_eigenvalues, permutation, axis=-1)
         diff = np.diff(covariance_eigenvalues, axis=-1)
-        eps = covariance_eigenvalues[..., -1] * eps
-        diff = np.maximum(diff, eps[..., None])
+        # eps = covariance_eigenvalues[..., -1] * eps
+        # diff = np.maximum(diff, eps[..., None])
+        diff = np.maximum(diff, eps)
 
         # This reconstruction is not optimal, but an error of 1e-8
-        covariance_eigenvalues[..., :-1] = (
-                covariance_eigenvalues[..., -1][..., None]
-                - np.cumsum(diff[..., ::-1], axis=-1)[..., ::-1]
+        covariance_eigenvalues[..., 1:] = (
+                covariance_eigenvalues[..., 0][..., None]
+                + np.cumsum(diff, axis=-1)
         )
 
         # https://stackoverflow.com/a/55737198/5766934
@@ -186,6 +205,8 @@ class ComplexBinghamTrainer:
     def __init__(
             self,
             dimension=None,
+            max_concentration=np.inf,
+            eignevalue_eps=1e-8,
     ):
         """
 
@@ -195,9 +216,13 @@ class ComplexBinghamTrainer:
                 function is called.
         """
         self.dimension = dimension
+        assert max_concentration > 0, max_concentration
+        self.max_concentration = max_concentration
+        self.eignevalue_eps = eignevalue_eps
 
     @classmethod
-    def find_eigenvalues_v2(cls, scatter_eigenvalues, eps=1e-8):
+    def find_eigenvalues_v2(cls, scatter_eigenvalues, eps=1e-8,
+                            max_concentration=np.inf):
         """
 
         This implementation uses a modified version of the generated files from
@@ -220,6 +245,12 @@ class ComplexBinghamTrainer:
         ...     3.74437619e-02, 9.44673748e-01], eps=1e-8)
         array([-1937.99743489, -1590.31683812,  -726.98624711,   -65.09507073,
                  -26.70671827,     0.        ])
+
+        >>> ComplexBinghamTrainer.find_eigenvalues_v2([
+        ...     5.15996555e-04, 6.28805516e-04, 1.37554184e-03, 1.53621463e-02,
+        ...     3.74437619e-02, 9.44673748e-01], max_concentration=500)
+        array([-500.        , -499.99999026, -499.99994411,  -70.4113198 ,
+                -27.56045117,    0.        ])
         """
         inverse_permutation, scatter_eigenvalues = ComplexBingham._remove_duplicate_eigenvalues(
             np.array(scatter_eigenvalues), eps=eps
@@ -233,6 +264,17 @@ class ComplexBinghamTrainer:
 
         x0 = -1 / scatter_eigenvalues
         x0[..., -1] = 0
+
+        if np.isinf(max_concentration):
+            pass
+        else:
+            x0 = np.maximum(
+                x0,
+                [
+                    -(max_concentration - d)
+                    for d in range(x0.shape[-1])
+                ]
+            )
         try:
             res = least_squares(
                 foo,
@@ -240,7 +282,7 @@ class ComplexBinghamTrainer:
                 x0[..., :-1],
                 # The largest eigenvalue is set to one, so all others have to
                 # be negative.
-                bounds=(-np.inf, 0),
+                bounds=(-max_concentration, 0),
                 kwargs={'scatter_eigenvalue': scatter_eigenvalues}
             )
         except ValueError as e:
@@ -254,6 +296,130 @@ class ComplexBinghamTrainer:
         # np.take_along_axis(covariance_eigenvalues, inverse_permutation,
         #                    axis=-1)
         return est[inverse_permutation]
+
+    @classmethod
+    def find_eigenvalues_v3(cls, scatter_eigenvalues, eps=1e-8,
+                            max_concentration=np.inf):
+        """
+
+        This implementation uses a modified version of the generated files from
+        https://github.com/libDirectional/libDirectional/tree/master/lib/util/autogenerated
+
+        ToDo: Generate the source code with python instead of MATLAB.
+
+        >>> ComplexBinghamTrainer.find_eigenvalues_v3([0.9, 0.1])
+        array([ 0.        , -9.99544117])
+        >>> ComplexBinghamTrainer.find_eigenvalues_v3([0.5, 0.5])
+        array([-0.00043799,  0.        ])
+
+        >>> ComplexBinghamTrainer.find_eigenvalues_v3([0.9, 0.06, 0.04])
+        array([  0.        , -16.66662429, -24.99999135])
+
+        >>> from pb_bss.distribution.complex_watson import ComplexWatsonTrainer
+        >>> t = ComplexWatsonTrainer(dimension=3)
+        >>> t.fill_hypergeometric_ratio_inverse([0.9, 0.06, 0.04])
+        array([ 19.99999117, -15.51872617, -23.90871118])
+
+        >>> grad_log_norm_symbolic[3](0.        , -16.66662429, -24.99999135)
+        [0.8999999999830024, 0.06000000001292287, 0.04000000000407487]
+        >>> grad_log_norm_symbolic[3](19.99999117, -15.51872617, -23.90871118)
+
+        >>> ComplexBinghamTrainer.find_eigenvalues_v3([0.9, 0.05, 0.05])
+        array([  0.      , -20.      , -19.999996])
+        >>> ComplexBinghamTrainer.find_eigenvalues_v3([0.9, 0.0666666667, 0.0333333333])
+        array([  0.        , -14.9998715 , -29.99998167])
+        >>> t.fill_hypergeometric_ratio_inverse([.9, 0.0666666667, 0.0333333333])
+        array([ 19.99999117, -13.83096999, -28.92572221])
+        >>> (t.fill_hypergeometric_ratio_inverse([.9, 0.0666666667, 0.0333333333]) - 20)**2
+        array([-8.82593048e-06, -3.38309700e+01, -4.89257222e+01])
+
+
+        >>> t.hypergeometric_ratio(20)
+        0.9000000412230742
+        >>> grad_log_norm_symbolic[3](20, 1e-8, 1e-9)
+        [0.900000041195574, 0.049999878940545125, 0.050000085055916174]
+
+        >>> t = ComplexWatsonTrainer(dimension=2)
+        >>> t.fill_hypergeometric_ratio_inverse([0.9, 0.1])
+        array([ 9.99544188, -9.99544188])
+
+
+
+        >>> ComplexBinghamTrainer.find_eigenvalues_v3([0.9, 0.06, 0.03, 0.006, 0.003, 0.001])
+        array([   0.        ,  -16.66663119,  -33.33332875, -166.66666412,
+               -333.33333091, -999.9999976 ])
+
+        >>> ComplexBinghamTrainer.find_eigenvalues_v3([
+        ...     5.15996555e-04, 6.28805516e-04, 1.37554184e-03, 1.53621463e-02,
+        ...     3.74437619e-02, 9.44673748e-01], eps=1e-8)
+        array([-1937.99743489, -1590.31683812,  -726.98624711,   -65.09507073,
+                 -26.70671827,     0.        ])
+
+        >>> ComplexBinghamTrainer.find_eigenvalues_v3([
+        ...     5.15996555e-04, 6.28805516e-04, 1.37554184e-03, 1.53621463e-02,
+        ...     3.74437619e-02, 9.44673748e-01], max_concentration=500)
+        array([-500.00000002, -500.00000001, -500.        ,  -66.3119293 ,
+                -26.90062851,    0.        ])
+        """
+        inverse_permutation, scatter_eigenvalues = ComplexBingham._remove_duplicate_eigenvalues(
+            np.array(scatter_eigenvalues), eps=eps
+        )
+
+        grad_log_norm_symbolic_diff_d = grad_log_norm_symbolic_diff[scatter_eigenvalues.shape[-1]]
+
+        def foo(x, scatter_eigenvalue):
+            ret = grad_log_norm_symbolic_diff_d(*x, 0) - scatter_eigenvalue
+            return ret
+
+        x0 = -1 / scatter_eigenvalues
+        x0[..., -1] = 0
+
+        if np.isinf(max_concentration):
+            pass
+        else:
+            x0 = np.maximum(
+                x0,
+                [
+                    -(max_concentration - d)
+                    for d in range(x0.shape[-1])
+                ]
+            )
+
+        # print(x0)
+        # Remove the degree of freedom
+        # Force order in using the diff
+        x0 = -np.diff(x0)
+        # print(x0)
+
+        try:
+            res = least_squares(
+                foo,
+                x0,  # the last value was removed in the np.diff
+                # The largest eigenvalue is set to one, so all others have to
+                # be negative.
+                bounds=(-max_concentration, -1e-8),
+                kwargs={'scatter_eigenvalue': scatter_eigenvalues}
+            )
+        except ValueError as e:
+            raise ValueError(x0, scatter_eigenvalues) from e
+
+        # Append the dropped eigenvalue
+        est = np.cumsum(np.array([*res.x, 0])[..., ::-1])[..., ::-1]
+
+        # `np.take_along_axis` not necessary, because this function does not
+        # support independent axes.
+        # np.take_along_axis(covariance_eigenvalues, inverse_permutation,
+        #                    axis=-1)
+        est = est[inverse_permutation]
+        if np.isinf(max_concentration):
+            return est
+        else:
+            est = np.maximum(est, -max_concentration)
+            inverse_permutation, est = ComplexBingham._remove_duplicate_eigenvalues(
+                est, eps=eps
+            )
+            return est[inverse_permutation]
+
 
     def find_eigenvalues_sympy(self, scatter_eigenvalues, start=None):
         """
@@ -405,12 +571,32 @@ class ComplexBinghamTrainer:
             denominator = np.einsum("...n->...", saliency)[..., None, None]
 
         covariance /= denominator
+        covariance = force_hermitian(covariance)
         scatter_eigenvalues, eigenvecs = np.linalg.eigh(covariance)
 
         eigenvalues = np.empty_like(scatter_eigenvalues)
         for index in np.ndindex(scatter_eigenvalues.shape[:-1]):
-            eigenvalues[index] = self.find_eigenvalues_v2(scatter_eigenvalues[index])
+            assert np.all(scatter_eigenvalues[index] >= 0), scatter_eigenvalues[index]
+            eigenvalues[index] = self.find_eigenvalues_v3(
+                scatter_eigenvalues[index],
+                max_concentration=self.max_concentration,
+                eps=self.eignevalue_eps
+            )
         return ComplexBingham(
             covariance_eigenvectors=eigenvecs,
             covariance_eigenvalues=eigenvalues,
         )
+
+
+def force_hermitian(matrix):
+    """
+
+    >>> A = np.array([[1+2j, 3+5j], [7+11j, 13+17j]])
+    >>> force_hermitian(A)
+    array([[ 1.+0.j,  5.-3.j],
+           [ 5.+3.j, 13.+0.j]])
+    >>> force_hermitian(force_hermitian(A))
+    array([[ 1.+0.j,  5.-3.j],
+           [ 5.+3.j, 13.+0.j]])
+    """
+    return (matrix + np.swapaxes(matrix.conj(), -1, -2)) / 2
