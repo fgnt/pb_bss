cc1df28e21d1114db02d1193b529b470e25fc1ce
Christoph <cbj@mail.uni-paderborn.de>
2019-09-24 15:17:10 +0200

    remove deleted imports

--- pb_bss/extraction/__init__.py
+++ pb_bss/extraction/__init__.py
@@ -1,7 +1,4 @@
 from .mask_module import *
 from .beamformer import *
-from .noise import *
 from .beamformer_wrapper import get_bf_vector
 from .beamformer_wrapper import get_bf_vector as get_single_source_bf_vector
-from .beamformer_wrapper import get_multi_source_bf_vector
-from .beamformer_wrapper import get_multi_source_bf_vector_from_masks
