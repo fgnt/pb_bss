cb61d7488f26bb4f9edb84b9baddc1c846f0de07
Christoph <cbj@mail.uni-paderborn.de>
2019-09-24 14:58:48 +0200

    add abs_square

--- pb_bss/utils.py
+++ pb_bss/utils.py
@@ -364,6 +364,31 @@ def labels_to_one_hot(
     return zeros
 
 
+def abs_square(x: np.ndarray):
+    """
+
+    https://github.com/numpy/numpy/issues/9679
+
+    Bug in numpy 1.13.1
+    >> np.ones(32768).imag ** 2
+    Traceback (most recent call last):
+    ...
+    ValueError: output array is read-only
+    >> np.ones(32767).imag ** 2
+    array([ 0.,  0.,  0., ...,  0.,  0.,  0.])
+
+    >>> abs_square(np.ones(32768)).shape
+    (32768,)
+    >>> abs_square(np.ones(32768, dtype=np.complex64)).shape
+    (32768,)
+    """
+
+    if np.iscomplexobj(x):
+        return x.real ** 2 + x.imag ** 2
+    else:
+        return x ** 2
+
+
 def unsqueeze(array, axis):
     """
 
