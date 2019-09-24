41264a09007afb1919b10979476aafedd20addbe
Christoph <cbj@mail.uni-paderborn.de>
2019-09-24 15:02:30 +0200

    remove german comments

--- pb_bss/extraction/mask_module.py
+++ pb_bss/extraction/mask_module.py
@@ -29,8 +29,7 @@ All other axes are regarded as independent dimensions.
 # TODO:  - nt/speech_enhancement/merl_masks.py
 # TODO:  - nt/speech_enhancement/mask_estimation.py
 # TODO: Add test-case for LorenzMask
-# CB: Eventuell einen Dekorator nutzen für force signal np.ndarray?
-# CB: Eventuell einen Dekorator nutzen für force signal.real.dtype == return.dtype?
+
 import numpy as np
 from typing import Optional
 from paderbox.math.misc import abs_square
@@ -49,6 +48,8 @@ __all__ = [
     'quantile_mask',
     'biased_binary_mask',
 ]
+
+
 
 
 def voiced_unvoiced_split_characteristic(
