e3533f7d837127ec8d5ead4cc1925876c2399e98
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2019-12-16 15:21:04 +0100

    spelling

--- tests/test_evaluation/test_pesq.py
+++ tests/test_evaluation/test_pesq.py
@@ -14,7 +14,7 @@ class TestProposedPESQ(unittest.TestCase):
         self.deg_array = pb.io.load_audio(self.deg_path)
 
     def test_wb_scores_with_lists_of_paths_length_one(self):
-        # ToDo: pesq does not support filesnames in the moment
+        # ToDo: pesq does not support filenames in the moment
         return
         scores = pesq(
             [self.ref_path],
@@ -24,7 +24,7 @@ class TestProposedPESQ(unittest.TestCase):
         np.testing.assert_allclose(scores, np.asarray([1.083]))
 
     def test_wb_scores_with_lists_of_paths_length_two(self):
-        # ToDo: pesq does not support filesnames in the moment
+        # ToDo: pesq does not support filenames in the moment
         return
         scores = pesq(
             [self.ref_path, self.ref_path],
@@ -50,7 +50,7 @@ class TestProposedPESQ(unittest.TestCase):
         np.testing.assert_allclose(scores, np.asarray([1.083234, 4.643888]), rtol=1e-6)
 
     def test_nb_scores_with_lists_of_paths_length_one(self):
-        # ToDo: pesq does not support filesnames in the moment
+        # ToDo: pesq does not support filenames in the moment
         return
         scores = pesq(
             [self.ref_path],
@@ -61,7 +61,7 @@ class TestProposedPESQ(unittest.TestCase):
         np.testing.assert_allclose(scores, np.asarray([1.607]))
 
     def test_nb_scores_with_lists_of_paths_length_two(self):
-        # ToDo: pesq does not support filesnames in the moment
+        # ToDo: pesq does not support filenames in the moment
         return
         scores = pesq(
             [self.ref_path, self.ref_path],
@@ -90,7 +90,7 @@ class TestProposedPESQ(unittest.TestCase):
         np.testing.assert_allclose(scores, np.asarray([1.607208, 4.548638]))
 
     def test_wb_scores_with_paths_directly(self):
-        # ToDo: pesq does not support filesnames in the moment
+        # ToDo: pesq does not support filenames in the moment
         return
         scores = pesq(
             self.ref_path,
@@ -100,7 +100,7 @@ class TestProposedPESQ(unittest.TestCase):
         np.testing.assert_allclose(scores, np.asarray([1.083]))
 
     def test_wrong_file(self):
-        # ToDo: pesq does not support filesnames in the moment
+        # ToDo: pesq does not support filenames in the moment
         return
         with self.assertRaisesRegex(
                 ChildProcessError,
